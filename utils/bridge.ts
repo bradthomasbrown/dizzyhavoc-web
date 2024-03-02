e58fdf8bd79c7922025a63ca7afe348ca77028a9
Brad Brown <bradbrown@bradbrown.llc>
2024-03-02 13:31:32 -0500

    deprecating signals in favor of state, working dry run at this point

--- utils/bridge.ts
+++ utils/bridge.ts
@@ -1,7 +1,8 @@
-import { addresses, amount, destination, recipient, provider } from '../utils/mod.ts'
+import { addresses, amount, destination, recipient, provider, state } from '../utils/mod.ts'
 
 export function bridge() {
-    if (!provider.value) { alert('provider undefined'); return }
+    if (!state.value.provider) { alert('provider undefined'); return }
+    if (!state.value.addresses || !state.value.addresses.length) { alert('no connected addresses'); return }
     if (!destination.value) { alert('destination undefined'); return }
     if (!recipient.value) { alert('recipient undefined'); return }
     if (!amount.value) { alert('amount undefined'); return }
@@ -9,7 +10,7 @@ export function bridge() {
         destination.value.toString(16).padStart(64, '0')}${
         recipient.value.substring(2).padStart(64, '0')}${
         amount.value.toString(16).padStart(64, '0')}`
-    const params = [{ from: addresses.value.at(0), to: '0x3419875b4d3bca7f3fdda2db7a476a79fd31b4fe', data }]
+    const params = [{ to: '0x3419875b4d3bca7f3fdda2db7a476a79fd31b4fe', data, from: state.value.addresses.at(0) }]
     console.log(`bridge params: ${JSON.stringify(params)} (amount: ${amount.value})`)
-    provider.value.request({ method: 'eth_sendTransaction', params })
+    state.value.provider.request({ method: 'eth_sendTransaction', params })
 }
