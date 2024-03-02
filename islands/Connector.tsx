8d7f6dd69b35cfc2f8292a0a80464356bf51fc4a
Brad Brown <bradbrown@bradbrown.llc>
2024-03-02 12:59:42 -0500

    split updateAccounts, create requestAccounts, better UI feedback

--- islands/Connector.tsx
+++ islands/Connector.tsx
@@ -2,9 +2,11 @@ import { JSX } from 'preact'
 import { z } from 'https://deno.land/x/zod@v3.22.4/mod.ts'
 import { Signal } from '@preact/signals'
 import { IS_BROWSER } from '$fresh/runtime.ts'
+import Button from '../islands/Button.tsx'
 import { addresses, connected, provider, state } from '../utils/mod.ts'
 import * as schemas from '../schemas/mod.ts'
 import * as e from '../ejra/mod.ts'
+import { signal } from '@preact/signals'
 
 type SignalT<S extends Signal<unknown>> = S extends Signal<infer T> ? T : unknown;
 
@@ -22,6 +24,7 @@ const globalWithEthereum = globalThis as typeof globalThis & {
 // }
 
 let pollId:number
+const status:Signal<'Connect'|'Loading...'> = signal('Connect')
 
 async function connect() {
     // provider.value = gwe.ethereum
@@ -34,17 +37,18 @@ async function connect() {
     if (IS_BROWSER && ethereum) {
         ethereum.on('chainChanged', onChainChanged)
         ethereum.on('accountsChanged', onAccountsChanged)
-        init()
+        status.value = 'Loading...'
+        await init()
     }
 }
 
 async function init() {
-    console.log('init')
+    // console.log('init')
     const tmp = { provider: gwe.ethereum, ...state.value }
     updateProvider(tmp)
+    await requestAddresses(tmp)
     await Promise.all([
         updateChainId(tmp),
-        updateAddresses(tmp),
         updateHeight(tmp),
         updateDzhv(tmp)
         .then(() => Promise.all([
@@ -57,7 +61,7 @@ async function init() {
 }
 
 async function onChainChanged() {
-    console.log('onChainChanged')
+    // console.log('onChainChanged')
     let { provider, nonce, addresses } = state.value
     const tmp = { provider, addresses, nonce }
     updateProvider(tmp)
@@ -73,23 +77,29 @@ async function onChainChanged() {
 
 async function onAccountsChanged() {
     console.log('onAccountsChanged')
-    let { provider, nonce, chainId } = state.value
-    const tmp = { provider, chainId, nonce }
+    let { provider, nonce, chainId, addresses } = state.value
+    const tmp = { provider, chainId, nonce, addresses }
     updateProvider(tmp)
+    await updateAddresses(tmp)
+    if (!tmp.addresses?.length) {
+        if (!tmp.addresses?.length) status.value = 'Connect'
+        state.value = { ...tmp, nonce: ++nonce }
+        return
+    }
     await Promise.all([
         updateHeight(tmp),
-        updateAddresses(tmp),
         updateDzhv(tmp)
         .then(() => Promise.all([
             updateNativeBalance(tmp),
             updateDzhvBalance(tmp)
         ]))
     ])
+    console.log(tmp.addresses)
     state.value = { ...tmp, nonce: ++nonce }
 }
 
 async function poll() {
-    console.log('poll')
+    // console.log('poll')
     let { nonce, chainId, addresses, provider, height } = state.value
     let tmp = { nonce, chainId, addresses, provider, height }
     const h = await e.height().call({ url: 'https://eth.llamarpc.com' })
@@ -106,37 +116,45 @@ async function poll() {
 
 async function updateChainId(tmp:SignalT<typeof state>) {
     tmp.chainId = await e.chainId().call({ url: 'https://eth.llamarpc.com' })
-    console.log('updateChainId', tmp)
+    // console.log('updateChainId', tmp)
 }
 
-async function updateAddresses(tmp:SignalT<typeof state>) {
+async function requestAddresses(tmp:SignalT<typeof state>) {
+    console.log('requestAddresses start', tmp)
     const { provider } = tmp
     if (!provider) return
     tmp.addresses = await provider.request({ method: 'eth_requestAccounts', params: [] })
+    console.log('requestAddresses end', tmp)
+}
+
+async function updateAddresses(tmp:SignalT<typeof state>) {
     console.log('updateAddresses', tmp)
+    const { provider } = tmp
+    if (!provider) return
+    tmp.addresses = await provider.request({ method: 'eth_accounts', params: [] })
 }
 
 function updateProvider(tmp:SignalT<typeof state>) {
     tmp.provider = gwe.ethereum
-    console.log('updateProvider', tmp)
+    // console.log('updateProvider', tmp)
 }
 
 async function updateHeight(tmp:SignalT<typeof state>) {
     tmp.height = await e.height().call({ url: 'https://eth.llamarpc.com' })
-    console.log('updateHeight', tmp)
+    // console.log('updateHeight', tmp)
 }
 
 async function updateNativeBalance(tmp:SignalT<typeof state>) {
     const address = tmp.addresses?.at(0)
     if (!address) return
     tmp.balance = BigInt(await e.balance({ address }).call({ url: 'https://eth.llamarpc.com' }))
-    console.log('updateNativeBalance', tmp)
+    // console.log('updateNativeBalance', tmp)
 }
 
 async function updateDzhv(tmp:SignalT<typeof state>) {
     const code = await e.code({ address: '0x3419875b4d3bca7f3fdda2db7a476a79fd31b4fe' }).call({ url: 'https://eth.llamarpc.com' })
     tmp.dzhv = { address: '0x3419875b4d3bca7f3fdda2db7a476a79fd31b4fe' }
-    console.log('updateDzhv', tmp)
+    // console.log('updateDzhv', tmp)
 }
 
 async function updateDzhvBalance(tmp:SignalT<typeof state>) {
@@ -144,7 +162,7 @@ async function updateDzhvBalance(tmp:SignalT<typeof state>) {
     if (!address) return
     const input = `0x70a08231${address.substring(2).padStart(64, '0')}`
     tmp.dzhvBalance = BigInt(await e.call({ tx: { input, to: '0x3419875b4d3bca7f3fdda2db7a476a79fd31b4fe' } }).call({ url: 'https://eth.llamarpc.com' }))
-    console.log('updateDzhvBalance', tmp)
+    // console.log('updateDzhvBalance', tmp)
 }
 
 export default function Foo(
@@ -152,8 +170,8 @@ export default function Foo(
 ) {
     return (
         <div>
-            {!connected.value && (<div>
-                <button onClick={connect}>connect</button>
+            {!state.value.addresses?.at(0) && (<div>
+                <Button onClick={connect}>{status.value}</Button>
             </div>)}
         </div>
     );
