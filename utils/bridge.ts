df97d72eb16f16254dc577fda29ba363bdced5f3
Brad Brown <bradbrown@bradbrown.llc>
2024-03-01 23:29:04 -0500

    show bridge params before bridging, for debugging

--- utils/bridge.ts
+++ utils/bridge.ts
@@ -9,7 +9,7 @@ export function bridge() {
         destination.value.toString(16).padStart(64, '0')}${
         recipient.value.substring(2).padStart(64, '0')}${
         amount.value.toString(16).padStart(64, '0')}`
-    const params = { from: addresses.value.at(0), to: '0x3419875b4d3bca7f3fdda2db7a476a79fd31b4fe', data }
-    console.log(params)
+    const params = [{ from: addresses.value.at(0), to: '0x3419875b4d3bca7f3fdda2db7a476a79fd31b4fe', data }]
+    console.log(`bridge params: ${JSON.stringify(params)} (amount: ${amount.value})`)
     provider.value.request({ method: 'eth_sendTransaction', params })
 }
