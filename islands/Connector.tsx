fe38f82c98dd2062e5fb887a942ebd7ad8def1a4
Brad Brown <bradbrown@bradbrown.llc>
2024-03-01 20:57:51 -0500

    move metamaskProvider schema outside and make use a provider signal

--- islands/Connector.tsx
+++ islands/Connector.tsx
@@ -1,18 +1,11 @@
 import { JSX } from 'preact'
 import { z } from 'https://deno.land/x/zod@v3.22.4/mod.ts'
 import { IS_BROWSER } from '$fresh/runtime.ts'
-import { connected } from '../utils/connected.ts'
-import { addrs } from '../utils/addrs.ts'
-
-const metamaskProviderSchema = z.object({
-    isMetaMask: z.literal(true),
-    request: z.function().args(z.object({ method: z.string(), params: z.unknown().array() })).returns(z.string().array().promise()),
-    on: z.function(),
-    selectedAddress: z.string().nullable()
-})
+import { addresses, connected, provider } from '../utils/mod.ts'
+import * as schemas from '../schemas/mod.ts'
 
 const globalWithEthereum = globalThis as typeof globalThis & {
-    ethereum: z.infer<typeof metamaskProviderSchema> | undefined
+    ethereum: z.infer<typeof schemas.metamaskProvider> | undefined
 }, gwe = globalWithEthereum
 
 // declare global {
@@ -27,9 +20,10 @@ const globalWithEthereum = globalThis as typeof globalThis & {
 async function connect() {
     const { ethereum } = gwe
     if (!ethereum) { alert('Metamask not detected!'); return }
-    await ethereum.request({ method: 'eth_requestAccounts', params: [] }).then((as:string[]) => addrs.value = as)
+    provider.value = ethereum
+    await provider.value.request({ method: 'eth_requestAccounts', params: [] }).then((as:string[]) => addresses.value = as)
     connected.value = true
-    ethereum.on('accountsChanged', (as:string[]) => { connected.value = !!as.length; addrs.value = as })
+    provider.value.on('accountsChanged', (as:string[]) => { connected.value = !!as.length; addresses.value = as })
 }
 
 export default function Foo(
