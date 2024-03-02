1493fd59cbbbd8397a10dd52009cc75f5cc59f43
Brad Brown <bradbrown@bradbrown.llc>
2024-03-01 20:57:26 -0500

    bridgeui form handlers and chainid mapping

--- islands/BridgeUI.tsx
+++ islands/BridgeUI.tsx
@@ -1,8 +1,22 @@
 import Button from '../islands/Button.tsx'
 import Web3Input from '../islands/Web3Input.tsx'
 import { JSX } from 'preact'
-import { connected } from '../utils/connected.ts'
-import { addrs } from '../utils/addrs.ts'
+import { addresses, bridge, connected, destination } from '../utils/mod.ts'
+import { recipient } from "../utils/mod.ts";
+
+function onDestinationInput(e:JSX.TargetedEvent<HTMLInputElement>) {
+    const name = e.currentTarget.value
+    let chainId
+    switch (name) {
+        case 'ETH': chainId = 1n; break;
+        case 'AVAX': chainId = 43114n; break;
+        case 'BASE': chainId = 8453n; break;
+        case 'ARB': chainId = 42161n; break;
+        case 'BSC': chainId = 56n; break;
+        default: alert('invalid chain'); return
+    }
+    destination.value = chainId
+}
 
 export default function Foo(
     props: JSX.HTMLAttributes<HTMLButtonElement>
@@ -13,21 +27,21 @@ export default function Foo(
             {connected.value && (<div class="flex flex-col items-center gap-1">
              
                 {/* TODO - add "from" field that can be used to prompt switching of chains */}
-                <input list="chains" placeholder={'destination'}></input>
-                <input list="addrs" placeholder={'address'}></input>
+                <input onInput={onDestinationInput} list="chains" placeholder={'destination'}></input>
+                <input onInput={e => recipient.value = e.currentTarget.value} list="addrs" placeholder={'address'}></input>
                 <Web3Input placeholder="amount" maxVal={1000000000000000000000000n} decimals={18n}></Web3Input>
-                <Button>bridge</Button>
+                <Button onClick={bridge}>bridge</Button>
                 
                 <datalist id="chains">
-                <option value="ETH"></option>
-                <option value="AVAX"></option>
-                <option value="BASE"></option>
-                <option value="ARB"></option>
-                <option value="BSC"></option>
+                    <option value="ETH"></option>
+                    <option value="AVAX"></option>
+                    <option value="BASE"></option>
+                    <option value="ARB"></option>
+                    <option value="BSC"></option>
                 </datalist>
                 
                 <datalist id="addrs">
-                {addrs.value.map(addr => (<option value={addr}></option>))}
+                    {addresses.value.map(address => (<option value={address}></option>))}
                 </datalist>
                 
             </div>)}
