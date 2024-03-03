15c1144290f5cc239b1efe83854c45fcca5b9e93
Brad Brown <bradbrown@bradbrown.llc>
2024-03-02 23:39:53 -0500

    move DAppState type to a .d.ts

--- utils/state.ts
+++ utils/state.ts
@@ -3,18 +3,6 @@ import { signal } from '@preact/signals'
 import * as schemas from '../schemas/mod.ts'
 const foo = schemas.metamaskProvider.optional()
 
-type DAppState = {
-    provider:undefined|z.infer<typeof foo>|null,
-    chainId:undefined|bigint|null,
-    addresses:undefined|string[]|null,
-    height:undefined|bigint|null
-    balance:undefined|bigint|null,
-    dzhv:undefined|{ address:string }|null,
-    dzhvBalance:undefined|bigint|null,
-    rpc:undefined|string|null
-    nonce:undefined|bigint|null
-}
-
 export const state = signal<DAppState>({
     provider: undefined,
     chainId: undefined,
