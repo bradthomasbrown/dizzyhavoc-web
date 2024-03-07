3587fd102350e04b95b67987a234b3e52469d66c
Brad Brown <bradbrown@bradbrown.llc>
2024-03-07 16:19:55 -0500

    invalidate bridge islands for now during decimation

--- islands/BridgeUI.tsx
+++ islands/BridgeUI.tsx
@@ -1,61 +1,63 @@
-import Button from '../islands/Button.tsx'
-import Web3Input from '../islands/Web3Input.tsx'
-import { JSX } from 'preact'
-import { bridge, w3LabelConv, destination, state } from '../utils/mod.ts'
-import { recipient } from '../utils/mod.ts'
-import Toaster from "./Toaster.tsx";
-import { Signal } from "@preact/signals";
+// import Button from '../islands/Button.tsx'
+// import Web3Input from '../islands/Web3Input.tsx'
+// import { JSX } from 'preact'
+// import { bridge, w3LabelConv, destination, state } from '../lib/utils/mod.ts'
+// import { recipient } from '../lib/utils/mod.ts'
+// import Toaster from "./Toaster.tsx";
+// import { Signal } from "@preact/signals";
 
-function onDestinationInput(e:JSX.TargetedEvent<HTMLInputElement>) {
-    const name = e.currentTarget.value
-    let chainId
-    switch (name) {
-        case 'ETH': chainId = 1n; break;
-        case 'AVAX': chainId = 43114n; break;
-        case 'BASE': chainId = 8453n; break;
-        case 'ARB': chainId = 42161n; break;
-        case 'BSC': chainId = 56n; break;
-        default: alert(`invalid chain ${name}`); return
-    }
-    destination.value = chainId
-}
+// function onDestinationInput(e:JSX.TargetedEvent<HTMLInputElement>) {
+//     const name = e.currentTarget.value
+//     let chainId
+//     switch (name) {
+//         case 'ETH': chainId = 1n; break;
+//         case 'AVAX': chainId = 43114n; break;
+//         case 'BASE': chainId = 8453n; break;
+//         case 'ARB': chainId = 42161n; break;
+//         case 'BSC': chainId = 56n; break;
+//         default: alert(`invalid chain ${name}`); return
+//     }
+//     destination.value = chainId
+// }
 
-export default function BridgeUI(
-    props: JSX.HTMLAttributes<HTMLButtonElement>
-) {
-    const foo = new Signal()
-    state.subscribe(({ dzhvBalance }) => {
-        if (dzhvBalance === null || dzhvBalance === undefined) return
-        foo.value = <div>{dzhvBalance ? w3LabelConv({ big: dzhvBalance, dec: 18, sym: 'DZHV', tarLen: 16, maxExt: Infinity }) : '...'}</div>
-    })
-    return (
-        <div>
+// export default function BridgeUI(
+//     props: JSX.HTMLAttributes<HTMLButtonElement>
+// ) {
+//     const foo = new Signal()
+//     state.subscribe(({ dzhvBalance }) => {
+//         if (dzhvBalance === null || dzhvBalance === undefined) return
+//         foo.value = <div>{dzhvBalance ? w3LabelConv({ big: dzhvBalance, dec: 18, sym: 'DZHV', tarLen: 16, maxExt: Infinity }) : '...'}</div>
+//     })
+//     return (
+//         <div>
 
-            <Toaster/>
+//             <Toaster/>
         
-            {state.value.addresses?.at(0) && (<div class="flex flex-col items-center gap-1">
+//             {state.value.addresses?.at(0) && (<div class="flex flex-col items-center gap-1">
              
-                {/* TODO - add "from" field that can be used to prompt switching of chains */}
-                { foo }
-                <input onInput={onDestinationInput} list="chains" placeholder={'destination'}></input>
-                <input onInput={e => recipient.value = e.currentTarget.value} list="addrs" placeholder={'address'}></input>
-                <Web3Input placeholder="amount" maxVal={state.value.dzhvBalance} decimals={18n}></Web3Input>
-                <Button onClick={bridge}>bridge</Button>
+//                 {/* TODO - add "from" field that can be used to prompt switching of chains */}
+//                 { foo }
+//                 <input onInput={onDestinationInput} list="chains" placeholder={'destination'}></input>
+//                 <input onInput={e => recipient.value = e.currentTarget.value} list="addrs" placeholder={'address'}></input>
+//                 <Web3Input placeholder="amount" maxVal={state.value.dzhvBalance} decimals={18n}></Web3Input>
+//                 <Button onClick={bridge}>bridge</Button>
                 
-                <datalist id="chains">
-                    <option value="ETH"></option>
-                    <option value="AVAX"></option>
-                    <option value="BASE"></option>
-                    <option value="ARB"></option>
-                    <option value="BSC"></option>
-                </datalist>
+//                 <datalist id="chains">
+//                     <option value="ETH"></option>
+//                     <option value="AVAX"></option>
+//                     <option value="BASE"></option>
+//                     <option value="ARB"></option>
+//                     <option value="BSC"></option>
+//                 </datalist>
                 
-                <datalist id="addrs">
-                    {state.value.addresses.map(address => (<option value={address}></option>))}
-                </datalist>
+//                 <datalist id="addrs">
+//                     {state.value.addresses.map(address => (<option value={address}></option>))}
+//                 </datalist>
                 
-            </div>)}
+//             </div>)}
             
-        </div>
-    );
-}
+//         </div>
+//     );
+// }
+
+export default function () { return <></> }
--- islands/Toaster.tsx
+++ islands/Toaster.tsx
@@ -1,16 +1,18 @@
-import { JSX } from 'preact'
-import Toast from './Toast.tsx'
-import { IS_BROWSER } from '$fresh/runtime.ts'
-import { toasts } from '../utils/mod.ts'
-import z from "https://deno.land/x/zod@v3.22.4/index.ts";
-import { Signal } from "@preact/signals";
-import * as e from '../ejra/mod.ts'
-const bar = e.receipt({ hash: '' }).ejrrq.schema
+// import { JSX } from 'preact'
+// import Toast from './Toast.tsx'
+// import { IS_BROWSER } from '$fresh/runtime.ts'
+// import { toasts } from '../lib/utils/mod.ts'
+// import z from "https://deno.land/x/zod@v3.22.4/index.ts";
+// import { Signal } from "@preact/signals";
+// import * as e from '../lib/ejra/mod.ts'
+// const bar = e.receipt({ hash: '' }).ejrrq.schema
 
-export default function Toaster(props: JSX.HTMLAttributes<HTMLButtonElement>) {
-    return (
-        <div class="fixed flex flex-col gap-2 bottom-5 right-5">
-            {toasts.value.map(({ component }) => component)}
-        </div>
-    )
-}
+// export default function Toaster(props: JSX.HTMLAttributes<HTMLButtonElement>) {
+//     return (
+//         <div class="fixed flex flex-col gap-2 bottom-5 right-5">
+//             {toasts.value.map(({ component }) => component)}
+//         </div>
+//     )
+// }
+
+export default function () { return <></> }
