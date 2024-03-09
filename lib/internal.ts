b8931e3966f2001de830cdd5fd10cf7332f4745f
Brad Brown <bradbrown@bradbrown.llc>
2024-03-08 19:43:42 -0500

    module load order helper

---
+++ lib/internal.ts
@@ -0,0 +1,75 @@
+// format described https://stackoverflow.com/a/76717884
+// useful for managing module loading order, otherwise we get circular dependency issues
+
+// # lib/
+import {
+    schemas,
+    UState, InjectedProvider,
+    rpc, provider, addresses, balance, chainId, dzhv, dzhvBalance, height, state,
+    query, overrides, bridgeable,
+    hexshort, bridge, w3LabelConv, getStateNonce
+} from './internal.ts'
+// ## schemas/
+export * from './schemas/mod.ts'
+// ## types/
+export * from './types/DAppState.ts'
+export * from './types/Chain.ts'
+export * from './types/InjectedProvider.ts'
+// ## ejra/
+export * as e from './ejra/mod.ts'
+// ## state/
+export * from './state/rpc.ts'
+export * from './state/provider.ts'
+export * from './state/addresses.ts'
+export * from './state/balance.ts'
+export * from './state/chainId.ts'
+export * from './state/dzhv.ts'
+export * from './state/dzhvBalance.ts'
+export * from './state/height.ts'
+export * from './state/state.ts'
+// ## chains/
+export * from './chains/query.ts'
+export * from './chains/overrides.ts'
+export * from './chains/bridgeable.ts'
+// ## utils/
+export * from './utils/hexshort.ts'
+export * from './utils/bridge.ts'
+export * from './utils/w3LabelConv.ts'
+export * from './utils/getStateNonce.ts'
+
+// # components/
+import {
+    Button, Footer,
+    Available, Buy, Info, More, Roadmap, Secret,
+    UI
+} from './internal.ts'
+// ## common/
+export * from '../components/common/Button.tsx'
+export * from '../components/common/Footer.tsx'
+// ## index/
+export * from '../components/index/Available.tsx'
+export * from '../components/index/Buy.tsx'
+export * from '../components/index/Info.tsx'
+export * from '../components/index/More.tsx'
+export * from '../components/index/Roadmap.tsx'
+export * from '../components/index/Secret.tsx'
+// ## bridge/
+export * from '../components/bridge/UI.tsx'
+
+// # islands/
+import {
+    Connector, Balance, Web3Input, ListInput,
+    TokenData,
+    LegalsPopup, Form, Toaster
+} from './internal.ts'
+// ## common/
+export * from '../islands/common/Connector.tsx'
+export * from '../islands/common/Balance.tsx'
+export * from '../islands/common/Web3Input.tsx'
+export * from '../islands/common/ListInput.tsx'
+// ## index/
+export * from '../islands/index/TokenData.tsx'
+// ## bridge/
+export * from '../islands/bridge/LegalsPopup.tsx'
+export * from '../islands/bridge/Form.tsx'
+export * from '../islands/bridge/Toaster.tsx'
