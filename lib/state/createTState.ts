a43512c26681021b18ac7ea21a4afe608bdd6c3d
Brad Brown <bradbrown@bradbrown.llc>
2024-03-09 02:11:09 -0500

    terminate ttrack, comments, grab from ttrack, update ttrack

--- lib/state/createTState.ts
+++ lib/state/createTState.ts
@@ -1,15 +1,31 @@
 import { Signal } from "@preact/signals";
-import { TState, state, getStateNonce } from '../internal.ts'
+import { TState, state, getStateNonce, ttrack } from '../internal.ts'
+import { DAppState } from "../internal.ts";
 
 export function createTState(invalidates:string[]) {
+    
+    // terminate ttrack if possible
+    ttrack.value?.abortController.abort()
+
+    // build tstate, using ttrack.tstate if applicable
     const stateEntries = Object.entries(state)
     const tstateEntries = stateEntries.map(([k, x]) => {
+        // if x not signal (stateNonce), return same
         if (!(x instanceof Signal)) return [k, x]
+        // if k is in invalidates, invalidate it
         if (invalidates.includes(k)) return [k, undefined]
-        const { value } = x
-        return [k, value]
+        // if value is in TTrack'd TState, use that
+        const value = ttrack.value?.tstate[k as keyof DAppState]
+        if (value) return [k, value]
+        // otherwise, use UState value
+        return [k, x.value]
     }) as [keyof TState, TState[keyof TState]][]
     const tstate = Object.fromEntries(tstateEntries) as TState
     tstate.stateNonce = getStateNonce()
-    return tstate
+
+    // update ttrack, return tstate and abortController
+    const abortController = new AbortController()
+    ttrack.value = { tstate, abortController }
+    return { tstate, abortController }
+
 }
