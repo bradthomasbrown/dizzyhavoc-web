5d314c299a473e8ff25207e44c09681d3e5cceee
Brad Brown <bradbrown@bradbrown.llc>
2024-03-08 22:22:54 -0500

    add overrides Chain array

---
+++ lib/chains/overrides.ts
@@ -0,0 +1,30 @@
+export const overrides = [
+    {
+        name: 'Testnet 8545',
+        chain: 'T8545',
+        rpc: ['http://localhost:8545'],
+        chainId: 8545,
+        nativeCurrency: {
+            name: 'Ether',
+            symbol: 'ETH',
+            decimal: 18
+        },
+        explorers: [],
+        icon: 'ethereum',
+        shortName: 't5'
+    },
+    {
+        name: 'Testnet 8546',
+        chain: 'T8546',
+        rpc: ['http://localhost:8546'],
+        chainId: 8546,
+        nativeCurrency: {
+            name: 'Ether',
+            symbol: 'ETH',
+            decimal: 18
+        },
+        explorers: [],
+        icon: 'ethereum',
+        shortName: 't6'
+    }
+]
