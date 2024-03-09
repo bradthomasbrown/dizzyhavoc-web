ef6abc0eae0f78c1b697f2a12a41eae09ad07319
Brad Brown <bradbrown@bradbrown.llc>
2024-03-08 19:47:28 -0500

    use internal.ts

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,4 +1,4 @@
-import Footer from '../components/Footer.tsx';
+import { Footer } from '../lib/internal.ts';
 import { type PageProps } from "$fresh/server.ts";
 export default function App({ Component }: PageProps) {
   return (
