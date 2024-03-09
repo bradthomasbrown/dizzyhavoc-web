6c77072d3fc653084c8595cc7959fd79b9da981f
Brad Brown <bradbrown@bradbrown.llc>
2024-03-08 22:23:03 -0500

    add bridgeable

---
+++ lib/chains/bridgeable.ts
@@ -0,0 +1,14 @@
+import { IS_BROWSER } from "$fresh/runtime.ts";
+import { Chain, query } from '../internal.ts'
+
+// active chain IDs
+const ids = [
+    1n,     // ethereum,
+    56n,    // binance smart chain
+    8453n,  // base
+    42161n, // arbitrum
+    43114n, // avalanche,
+    8546n,  // local testnet 8546
+]
+
+export const bridgeable = !IS_BROWSER ? [] : await Promise.all(ids.map(id => query({ id }))) as Chain[]
