2d1b2dd6819b9879fe9f864519fcb4c10858f5dd
Corvardt <124924375+corvardt@users.noreply.github.com>
2024-03-21 00:14:28 +0100

    dark mode popup

--- islands/bridge/LegalsPopup.tsx
+++ islands/bridge/LegalsPopup.tsx
@@ -29,11 +29,11 @@ export function LegalsPopup() {
                              <div 
                              className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 z-50 flex items-center justify-center"
                          >
-                             <div className="bg-white h-auto p-6 rounded max-w-md mx-auto">
-                                 <p className="text-xl text-center">
+                             <div className="bg-white dark:bg-[#191919] h-auto p-6 rounded max-w-md mx-auto">
+                                 <p className="text-xl text-gray-800 dark:text-[#d2d2d2] text-center">
                                      By clicking continue and using our Bridge you agree to:
                                  </p>
-                                 <div className="mt-4 text-gray-800 text-base leading-6">
+                                 <div className="mt-4 text-gray-800 dark:text-[#d2d2d2] text-base leading-6">
                                      <ul className="list-disc ml-8">
                                          <li>
                                              Being blessed by the DizzyHavoc team   {/* ! to change before deploy ! */}
@@ -47,17 +47,17 @@ export function LegalsPopup() {
                                      <label className="inline-flex items-center">
                                          <input
                                              type="checkbox"
-                                             className="form-checkbox text-blue-600"
+                                             className="form-checkbox text-gray-800 dark:text-[#d2d2d2]"
                                              checked={checked}
                                              onChange={() => setchecked(!checked)}
                                          />
-                                         <span className="ml-2">I agree to these terms</span>
+                                         <span className="ml-2 text-gray-800 dark:text-[#d2d2d2]">I agree to these terms</span>
                                      </label>
                                  </div>
                                  <div className="mt-6 flex justify-end">
                                      <div
                                          style={{ opacity: checked ? 1 : 0.5, pointerEvents: checked ? 'auto' : 'none' }}
-                                         className={`text-2xl mx-auto text-[#3d3d3d] shadow-xl font-[Poppins] rounded-lg py-1 px-4 hover:scale-[105%] border border-[#e9e9e9] cursor-pointer bg-[#f1f1f1] ${displayPopUp ? '' : 'opacity-50 pointer-events-none'}`}
+                                         className={`text-2xl mx-auto shadow-xl font-[Poppins] text-gray-800 dark:text-[#d2d2d2] rounded-lg py-1 px-4 hover:scale-[105%] border dark:border-[#3d3d3d] border-[#e9e9e9] dark:bg-[#191919] cursor-pointer bg-[#f1f1f1] ${displayPopUp ? '' : 'opacity-50 pointer-events-none'}`}
                                          onClick={() => {closePopUp()}}
                                      >
                                          Continue
