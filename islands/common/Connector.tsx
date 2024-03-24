a4f3ec13ac5e568a7e828492db0fc9f324254d5b
Corvardt <124924375+corvardt@users.noreply.github.com>
2024-03-24 20:47:57 +0100

    .

--- islands/common/Connector.tsx
+++ islands/common/Connector.tsx
@@ -12,18 +12,19 @@ const globalWithEthereum = globalThis as typeof globalThis & {
 }, gwe = globalWithEthereum
 
 function connect() {
-    if (status.value == 'Connected') return
-    console.log('CONNECT')
-    const { ethereum } = gwe
-    if (ethereum) {
-        alert('CONNECTING')
-        ethereum.on('chainChanged', onChainChanged)
-        ethereum.on('accountsChanged', onAccountsChanged)
-        status.value = 'Loading'
-        init().then(() => { status.value = 'Connected' })
-    } else {
-        alert('NOPE')
-    }
+    alert('Pressed')
+    // if (status.value == 'Connected') return
+    // console.log('CONNECT')
+    // const { ethereum } = gwe
+    // if (IS_BROWSER && ethereum) {
+    //     alert('CONNECTING')
+    //     ethereum.on('chainChanged', onChainChanged)
+    //     ethereum.on('accountsChanged', onAccountsChanged)
+    //     status.value = 'Loading'
+    //     init().then(() => { status.value = 'Connected' })
+    // } else {
+    //     alert('NOPE')
+    // }
 }
 
 export function Connector() {
