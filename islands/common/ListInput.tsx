0b1dbd45b97772dc2e1ccc1f3edca3e86f12953b
Corvardt <124924375+corvardt@users.noreply.github.com>
2024-03-24 23:51:27 +0100

    format

--- islands/common/ListInput.tsx
+++ islands/common/ListInput.tsx
@@ -3,7 +3,7 @@ import { JSX } from "preact/jsx-runtime";
 export function ListInput(props:{ addClass?:string }&JSX.HTMLAttributes<HTMLInputElement>) {
     return (
         <input
-            class={`py-2 text-center text-[#2c2c2c] border dark:border-1 border-1 border-[#2c2c2c2a] dark:border-[#eaeaea2a] dark:text-[#EAEAEA] dark:placeholder:text-[#EAEAEA] max-w-[12rem] placeholder:text-[#2c2c2c] placeholder:text-sm bg-[#f2f2f2] dark:bg-[#1e1e1e] rounded-lg ${props.addClass ?? ''}`}
+            class={`py-2 text-center text-[#2c2c2c] border dark:border-1 border-1 border-[#2c2c2c2a] dark:border-[#eaeaea2a] dark:text-[#EAEAEA] dark:placeholder:text-[#EAEAEA] max-w-[12rem] max-h-[2rem] placeholder:text-[#2c2c2c] placeholder:text-sm bg-[#f2f2f2] dark:bg-[#1e1e1e] rounded-lg ${props.addClass ?? ''}`}
             onInput={props.onInput}
             list={props.list}
             placeholder={props.placeholder}
