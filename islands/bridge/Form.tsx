42576463c4d4f640f7c02ee721f5125bec10ed3e
Corvardt <124924375+corvardt@users.noreply.github.com>
2024-03-24 23:17:43 +0100

    layout test

--- islands/bridge/Form.tsx
+++ islands/bridge/Form.tsx
@@ -69,10 +69,9 @@ export function Form() {
 <div class="flex absolute flex-col top-0 left-0 items-start">
         {<img class="size-[2.2rem] rounded-sm mt-6 ml-6" src={blockieSrc} title={adrs} alt="blockie image"></img>}
         <div class="font-[Poppins] text-[#2c2c2c] dark:text-[#EAEAEA] font-sm ml-6">{hexshortSelected}</div>
-    </div>
-       <div class="flex flex-col items-center justify-center translate-x-[0%] sm:translate-x-[25%] gap-1 w-full h-full p-4 text-[#2c2c2c] dark:text-[#EAEAEA] font-[Poppins]">
-  
-    <div class="flex flex-col items-center mt-[5rem] justify-center gap-1">
+</div>
+<div class="flex flex-col items-center text-[#2c2c2c] dark:text-[#EAEAEA] font-[Poppins]">
+    <div class="flex flex-col items-center mt-[4rem]">
         <Balance/>
         <Web3Input placeholder="amount" maxVal={dzhvBalance.value} decimals={18n} val={amount}/>
         <div class="font-[Poppins] text-[#2c2c2c] dark:text-[#EAEAEA] font-medium mb-4">to:</div>
