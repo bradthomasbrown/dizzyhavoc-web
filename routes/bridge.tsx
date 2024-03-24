c18aebea6d4b0fbc82dcfad585fd599c7b3f7c38
Corvardt <124924375+corvardt@users.noreply.github.com>
2024-03-24 18:57:38 +0100

    popup test

--- routes/bridge.tsx
+++ routes/bridge.tsx
@@ -1,4 +1,4 @@
-import { LegalsPopup, Connector, UI, Footer } from '../lib/internal.ts'
+import {  Connector, UI, Footer } from '../lib/internal.ts'
 import { Head } from "$fresh/runtime.ts";
 export default function Home() {
   return (
@@ -7,7 +7,7 @@ export default function Home() {
         <title>Vertigo</title>
         <link rel="icon" href="/misc/Vertigo2.png" />
       </Head>
-      <LegalsPopup />
+      {/* <LegalsPopup /> */}
       <div class="sm:min-h-[4rem] min-h-[4.5rem] w-full sm:bg-transparent dark:sm:bg-transparent dark:bg-[#2828283f] bg-[#dbdbdb3f] sm:w-[45rem] justify-center mx-auto items-center px-4 flex  border-e-transparent border-s-transparent border-t-transparent dark:border-e-transparent dark:border-s-transparent dark:border-t-transparent dark:border-[#5e5e5e4d] border-[#dbdbdb] border">
       <div class="justify-start w-full flex">
         <div class="lg:text-[1.8rem] unselectable sm:gap-2 gap-1 items-center flex justify-center text-[1.5rem] font-[Poppins] font-medium z-10 font-Poppins dark:text-[#d2d2d2] text-[#282828]">
