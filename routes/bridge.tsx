f8a0654acfd822075ca986cff40886b3b8449649
Corvardt <124924375+corvardt@users.noreply.github.com>
2024-03-24 22:09:35 +0100

    layout

--- routes/bridge.tsx
+++ routes/bridge.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       {/* <LegalsPopup /> */}
       <div class="w-full h-screen flex justify-center">
         <div class="flex w-full flex-col items-center mb-[5rem]">
-          <div className="min-w-[85%] sm:min-w-[500px] bg-blur2 shadow-xl rounded-xl flex min-h-[25rem] my-auto">
+          <div className="min-w-[340px] sm:min-w-[500px] bg-blur2 shadow-xl rounded-xl flex min-h-[25rem] my-auto">
             <div class="flex flex-col lg:flex-row">
               <UI/>
             </div>
