383f6dae702e22b41a2b16bbbc6f9e1f525d1496
Corvardt <124924375+corvardt@users.noreply.github.com>
2024-03-26 13:58:21 +0100

    navbar

--- routes/faucet.tsx
+++ routes/faucet.tsx
@@ -1,18 +1,11 @@
-import { Connector } from '../islands/common/Connector.tsx'
 import { Form } from '../components/faucet/Form.tsx'
 
 export default function Faucet() {
 
     return (
         <>
-
-            {/* header navbar-ish container */}
-            <div class="justify-end w-full flex p-2"> 
-                <Connector/>
-            </div>
-
             {/*grow to keep footer at bottom, includes title and app form*/}
-            <div class="grow flex flex-col justify-center"> 
+            <div class="grow h-screen flex flex-col justify-center"> 
 
                 {/* app title */}
                 <div class="lg:text-[1.8rem] unselectable text-[1.5rem] font-[Poppins] font-medium dark:text-[#d2d2d2] text-[#282828]">
