640e0139e308144f095607a9100a8c75e6a2865f
Corvardt <124924375+corvardt@users.noreply.github.com>
2024-03-26 23:46:08 +0100

    weird

--- routes/bridge.tsx
+++ routes/bridge.tsx
@@ -1,11 +1,9 @@
 import { UI } from '../lib/internal.ts'
-import { NavBar } from "../lib/internal.ts";
 import { Head } from "$fresh/runtime.ts";
 import { LegalsPopup } from "../islands/bridge/LegalsPopup.tsx";
 export default function Home() {
   return (
     <>
-    <NavBar/>
     <div class="h-screen sm:mb-[230px] mb-[100px] flex flex-col">
          <Head>
         <title>Vertigo</title>
--- routes/faucet.tsx
+++ routes/faucet.tsx
@@ -1,21 +1,15 @@
 import { Form } from '../components/faucet/Form.tsx'
-import { NavBar } from '../lib/internal.ts'
 
 export default function Faucet() {
 
     return (
         <>
-        <NavBar/>
         <div class="h-screen sm:mb-[230px] mb-[100px] flex flex-col">
-        
             {/*grow to keep footer at bottom, includes title and app form*/}
             <div class="grow h-screen flex flex-col w-full justify-center"> 
-
                 {/* app form */}
                 <Form/> 
-
-            </div>
-            
+            </div>  
         </div>
         </>
     )
