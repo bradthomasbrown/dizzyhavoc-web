36ac2a50d6da82a340c87ffb64d5a2601ed3f84f
Corvardt <124924375+corvardt@users.noreply.github.com>
2024-03-26 19:18:15 +0100

    mobile navbar test

--- routes/faucet.tsx
+++ routes/faucet.tsx
@@ -5,7 +5,7 @@ export default function Faucet() {
     return (
         <>
             {/*grow to keep footer at bottom, includes title and app form*/}
-            <div class="grow h-screen flex flex-col justify-center"> 
+            <div class="grow h-screen flex flex-col items-center justify-center"> 
 
                 {/* app title */}
                 <div class="lg:text-[1.8rem] unselectable text-[1.5rem] font-[Poppins] font-medium dark:text-[#d2d2d2] text-[#282828]">
