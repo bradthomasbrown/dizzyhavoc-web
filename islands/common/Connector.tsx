371767dac6bc3216b6a7cba19cd08a229cde1a35
Brad Brown <bradbrown@bradbrown.llc>
2024-03-26 01:08:14 -0400

    connector vortex overhaul

--- islands/common/Connector.tsx
+++ islands/common/Connector.tsx
@@ -1,31 +1,40 @@
-import { IS_BROWSER } from "$fresh/runtime.ts";
-import { Signal, signal } from "@preact/signals";
-import {
-    init, poll, onChainChanged, onAccountsChanged,
-    Button, InjectedProvider
-} from "../../lib/internal.ts";
+import { Signal, signal, computed } from '@preact/signals'
+import { Button } from '../../components/common/Button.tsx'
+import { getG1193, wp1193 } from '../../lib/state2/1193.ts'
+import { vortex } from '../../lib/faucet/vortex.ts'
 
-const status:Signal<'Connect'|'Loading'|'Connected'> = signal('Connect')
+export const status = computed(() => {
+    const uAddresses = vortex.uState.addresses.value
+    const updaters = vortex.updaters.value
+    const updater = vortex.data.addresses.updater
+    const tAddresses = vortex.tState.addresses
+    if (uAddresses && !(uAddresses instanceof Error)) return 'Connected'
+    if (updaters.has(updater)) return 'Connecting'
+    if ((tAddresses && !(tAddresses instanceof Error))) return 'Loading'
+    if (
+        (!uAddresses || uAddresses instanceof Error)
+        && (!tAddresses || tAddresses instanceof Error)
+        && !updaters.has(updater)
+    ) return 'Connect'
+    return `?${
+            !uAddresses ? 0 : uAddresses instanceof Error ? 1 : 2
+        }${ !updaters.has(updater) ? 0 : 2
+        }${ !tAddresses ? 0 : tAddresses instanceof Error ? 1 : 2}`
+})
 
-const globalWithEthereum = globalThis as typeof globalThis & {
-    ethereum?: InjectedProvider
-}, gwe = globalWithEthereum
+const disabled = computed(() => status.value != 'Connect')
 
 function connect() {
-    if (status.value == 'Connected') return
-    console.log('CONNECT')
-    const { ethereum } = gwe
-    if (IS_BROWSER && ethereum) {
-        ethereum.on('chainChanged', onChainChanged)
-        ethereum.on('accountsChanged', onAccountsChanged)
-        status.value = 'Loading'
-        init().then(() => { status.value = 'Connected' })
-    }
+    const g1193 = getG1193()
+    if (!g1193.ethereum) { alert('no eip-1193 provider detected'); return }
+    wp1193.onChainChanged(() => vortex.flow('chain'))
+    wp1193.onAccountsChanged(() => vortex.flow('account'))
+    vortex.flow('init')
 }
 
 export function Connector() {
     return (
-        <Button addClass="text-2xl px-2 text-center w-[180px] shadow-xl" onClick={connect}>
+        <Button disabled={disabled.value} addClass="text-2xl px-2 text-center w-[180px] shadow-xl" onClick={disabled.value ? () => {} : connect}>
             {status}
         </Button>
     );
