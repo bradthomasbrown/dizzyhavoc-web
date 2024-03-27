e887b3f1aff858cc672f5ee729bb912814da963c
Brad Brown <bradbrown@bradbrown.llc>
2024-03-27 00:38:45 -0400

    overhaul provider datum updater and some other things

--- lib/faucet/vortex.ts
+++ lib/faucet/vortex.ts
@@ -9,9 +9,10 @@ import {
     VortexData,
     TStateOperator
 } from '../state2/Vortex.ts'
-import { getG1193, wp1193, p1193 as sp1193, P1193 } from '../state2/1193.ts'
-import { EIP6963AnnounceProviderEvent } from '../state2/6963.ts'
+import { p1193 as sp1193, P1193, P5749, P6963, getG1193, EIP6963AnnounceProviderEvent } from '../state2/providers.ts'
 import { ejra } from './ejra.ts'
+import { Blockie } from '../blockies/Blockie.ts'
+import { choose } from '../../islands/common/Connector.tsx'
 
 const init:VortexFlow = async function() {
     
@@ -32,7 +33,7 @@ const init:VortexFlow = async function() {
             const { tState, updaters, flow } = this
             const key = this.dataKey.get(updater) as string
             const dependencies = this.dependencies.get(updater) as string[]
-            const operator = new TStateOperator({ tState, key, signal, updater, updaters })
+            const operator = new TStateOperator({ tState, key, signal, controller, updater, updaters })
             const datumUpdaterContext = { operator, dependencies, flow }
             return updater.bind(datumUpdaterContext)()
         }))
@@ -70,7 +71,7 @@ const chain:VortexFlow = async function() {
             const { tState, updaters, flow } = this
             const key = this.dataKey.get(updater) as string
             const dependencies = this.dependencies.get(updater) as string[]
-            const operator = new TStateOperator({ tState, key, signal, updater, updaters })
+            const operator = new TStateOperator({ tState, key, signal, controller, updater, updaters })
             const datumUpdaterContext = { operator, dependencies, flow }
             return updater.bind(datumUpdaterContext)()
         }))
@@ -108,7 +109,7 @@ const account:VortexFlow = async function() {
             const { tState, updaters, flow } = this
             const key = this.dataKey.get(updater) as string
             const dependencies = this.dependencies.get(updater) as string[]
-            const operator = new TStateOperator({ tState, key, signal, updater, updaters })
+            const operator = new TStateOperator({ tState, key, signal, controller, updater, updaters })
             const datumUpdaterContext = { operator, dependencies, flow }
             return updater.bind(datumUpdaterContext)()
         }))
@@ -146,7 +147,7 @@ const block:VortexFlow = async function() {
             const { tState, updaters, flow } = this
             const key = this.dataKey.get(updater) as string
             const dependencies = this.dependencies.get(updater) as string[]
-            const operator = new TStateOperator({ tState, key, signal, updater, updaters })
+            const operator = new TStateOperator({ tState, key, signal, controller, updater, updaters })
             const datumUpdaterContext = { operator, dependencies, flow }
             return updater.bind(datumUpdaterContext)()
         }))
@@ -173,109 +174,64 @@ const data = {
         dependsOn: [],
         async updater() {
             if (this.operator.get()) return
-            
-            const g1193 = getG1193()
-            const p1193 = g1193.ethereum
 
-            // # check for metamask mobile first
-            const mGate = new Gate<boolean>()
-            // then check if a mobile user has metamask's app installed
+            // get G1193
+            const g1193 = getG1193()
+
+            // create a map from uuid to provider
+            const p6963s = new Map<string,P6963>()
+            
+            // check standard 5749 
+            if (g1193.evmproviders) {
+                for (const provider of Object.values(g1193.evmproviders)) {
+                    const info = provider.info ?? { name: 'Unknown', uuid: crypto.randomUUID(), icon: new Blockie({ scale: 12, seed: [...crypto.getRandomValues(new Uint8Array(20))].map(x => String.fromCharCode(x)).join('') }).base64() }
+                    p6963s.set(info.uuid, { provider, info })
+                }
+            }
+            
+            // check nonstandard 5749 
+            if (g1193.providers) {
+                for (const provider of g1193.providers) {
+                    const info = provider.info ?? { name: 'Unknown', uuid: crypto.randomUUID(), icon: new Blockie({ scale: 12, seed: [...crypto.getRandomValues(new Uint8Array(20))].map(x => String.fromCharCode(x)).join('') }).base64() }
+                    p6963s.set(info.uuid, { provider, info })
+                }
+            }
+
+            // check trustwallet
+            if (g1193.trustwallet) {
+                const provider = g1193.trustwallet
+                const info = provider.info ?? { name: 'TrustWallet', uuid: crypto.randomUUID(), icon: new Blockie({ scale: 12, seed: [...crypto.getRandomValues(new Uint8Array(20))].map(x => String.fromCharCode(x)).join('') }).base64() }
+                p6963s.set(info.uuid, { provider, info })
+            }
+
+            // check 6963
+            globalThis.addEventListener('eip6963:announceProvider', (event:EIP6963AnnounceProviderEvent) => { p6963s.set(event.detail.info.uuid, { ...event.detail }) })
+            globalThis.dispatchEvent(new Event('eip6963:requestProvider'))
+
+            // if mobile, check MMSDK
             if (navigator.maxTouchPoints > 0) {
-                // create MMSDK
                 // @ts-ignore going to see if we can lazy load this since it is too fat for deno deploy
                 const { MetaMaskSDK } = (await import('https://unpkg.com/@metamask/sdk@0.18.1/dist/browser/umd/metamask-sdk.js')).default
-                const MMSDK = new MetaMaskSDK({
-                    dappMetadata: {
-                        name: "DZHV Testnet Faucet",
-                        url: window.location.href
-                    }
-                })
-                // https://docs.metamask.io/wallet/how-to/use-sdk/javascript/pure-js/, apparently setup is async
+                // according to metamask this is deceptive and not really synchronous
+                const MMSDK = new MetaMaskSDK({ dappMetadata: { name: "DZHV Testnet Faucet", url: window.location.href } })
+                // so try to get the provider after an immediate timeout, but use gate to make this somewhat more synchronous
+                const gate = new Gate<void>()
                 setTimeout(() => {
-                    // try to get its provider
-                    const p1193 = MMSDK.getProvider()
-                    // if got, resolve it
-                    if (!p1193) { mGate.resolve(false); return }
-                    const result = this.operator.set(p1193)
-                    if (result instanceof Error) { mGate.resolve(false); return }
-                    p1193.on('chainChanged', () => vortex.flow('chain'))
-                    p1193.on('accountsChanged', () => vortex.flow('account'))
-                    mGate.resolve(true)
+                    const provider = MMSDK.getProvider() as undefined|P1193&Partial<P5749>
+                    if (!provider) { gate.resolve(); return }
+                    const info = provider.info ?? { name: 'MetaMask', uuid: crypto.randomUUID(), icon: '"data:image/svg+xml,%3Csvg%20fill%3D%22none%22%20height%3D%2233%22%20viewBox%3D%220%200%2035%2033%22%20width%3D%2235%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%3E%3Cg%20stroke-linecap%3D%22round%22%20stroke-linejoin%3D%22round%22%20stroke-width%3D%22.25%22%3E%3Cpath%20d%3D%22m32.9582%201-13.1341%209.7183%202.4424-5.72731z%22%20fill%3D%22%23e17726%22%20stroke%3D%22%23e17726%22%2F%3E%3Cg%20fill%3D%22%23e27625%22%20stroke%3D%22%23e27625%22%3E%3Cpath%20d%3D%22m2.66296%201%2013.01714%209.809-2.3254-5.81802z%22%2F%3E%3Cpath%20d%3D%22m28.2295%2023.5335-3.4947%205.3386%207.4829%202.0603%202.1436-7.2823z%22%2F%3E%3Cpath%20d%3D%22m1.27281%2023.6501%202.13055%207.2823%207.46994-2.0603-3.48166-5.3386z%22%2F%3E%3Cpath%20d%3D%22m10.4706%2014.5149-2.0786%203.1358%207.405.3369-.2469-7.969z%22%2F%3E%3Cpath%20d%3D%22m25.1505%2014.5149-5.1575-4.58704-.1688%208.05974%207.4049-.3369z%22%2F%3E%3Cpath%20d%3D%22m10.8733%2028.8721%204.4819-2.1639-3.8583-3.0062z%22%2F%3E%3Cpath%20d%3D%22m20.2659%2026.7082%204.4689%202.1639-.6105-5.1701z%22%2F%3E%3C%2Fg%3E%3Cpath%20d%3D%22m24.7348%2028.8721-4.469-2.1639.3638%202.9025-.039%201.231z%22%20fill%3D%22%23d5bfb2%22%20stroke%3D%22%23d5bfb2%22%2F%3E%3Cpath%20d%3D%22m10.8732%2028.8721%204.1572%201.9696-.026-1.231.3508-2.9025z%22%20fill%3D%22%23d5bfb2%22%20stroke%3D%22%23d5bfb2%22%2F%3E%3Cpath%20d%3D%22m15.1084%2021.7842-3.7155-1.0884%202.6243-1.2051z%22%20fill%3D%22%23233447%22%20stroke%3D%22%23233447%22%2F%3E%3Cpath%20d%3D%22m20.5126%2021.7842%201.0913-2.2935%202.6372%201.2051z%22%20fill%3D%22%23233447%22%20stroke%3D%22%23233447%22%2F%3E%3Cpath%20d%3D%22m10.8733%2028.8721.6495-5.3386-4.13117.1167z%22%20fill%3D%22%23cc6228%22%20stroke%3D%22%23cc6228%22%2F%3E%3Cpath%20d%3D%22m24.0982%2023.5335.6366%205.3386%203.4946-5.2219z%22%20fill%3D%22%23cc6228%22%20stroke%3D%22%23cc6228%22%2F%3E%3Cpath%20d%3D%22m27.2291%2017.6507-7.405.3369.6885%203.7966%201.0913-2.2935%202.6372%201.2051z%22%20fill%3D%22%23cc6228%22%20stroke%3D%22%23cc6228%22%2F%3E%3Cpath%20d%3D%22m11.3929%2020.6958%202.6242-1.2051%201.0913%202.2935.6885-3.7966-7.40495-.3369z%22%20fill%3D%22%23cc6228%22%20stroke%3D%22%23cc6228%22%2F%3E%3Cpath%20d%3D%22m8.392%2017.6507%203.1049%206.0513-.1039-3.0062z%22%20fill%3D%22%23e27525%22%20stroke%3D%22%23e27525%22%2F%3E%3Cpath%20d%3D%22m24.2412%2020.6958-.1169%203.0062%203.1049-6.0513z%22%20fill%3D%22%23e27525%22%20stroke%3D%22%23e27525%22%2F%3E%3Cpath%20d%3D%22m15.797%2017.9876-.6886%203.7967.8704%204.4833.1949-5.9087z%22%20fill%3D%22%23e27525%22%20stroke%3D%22%23e27525%22%2F%3E%3Cpath%20d%3D%22m19.8242%2017.9876-.3638%202.3584.1819%205.9216.8704-4.4833z%22%20fill%3D%22%23e27525%22%20stroke%3D%22%23e27525%22%2F%3E%3Cpath%20d%3D%22m20.5127%2021.7842-.8704%204.4834.6236.4406%203.8584-3.0062.1169-3.0062z%22%20fill%3D%22%23f5841f%22%20stroke%3D%22%23f5841f%22%2F%3E%3Cpath%20d%3D%22m11.3929%2020.6958.104%203.0062%203.8583%203.0062.6236-.4406-.8704-4.4834z%22%20fill%3D%22%23f5841f%22%20stroke%3D%22%23f5841f%22%2F%3E%3Cpath%20d%3D%22m20.5906%2030.8417.039-1.231-.3378-.2851h-4.9626l-.3248.2851.026%201.231-4.1572-1.9696%201.4551%201.1921%202.9489%202.0344h5.0536l2.962-2.0344%201.442-1.1921z%22%20fill%3D%22%23c0ac9d%22%20stroke%3D%22%23c0ac9d%22%2F%3E%3Cpath%20d%3D%22m20.2659%2026.7082-.6236-.4406h-3.6635l-.6236.4406-.3508%202.9025.3248-.2851h4.9626l.3378.2851z%22%20fill%3D%22%23161616%22%20stroke%3D%22%23161616%22%2F%3E%3Cpath%20d%3D%22m33.5168%2011.3532%201.1043-5.36447-1.6629-4.98873-12.6923%209.3944%204.8846%204.1205%206.8983%202.0085%201.52-1.7752-.6626-.4795%201.0523-.9588-.8054-.622%201.0523-.8034z%22%20fill%3D%22%23763e1a%22%20stroke%3D%22%23763e1a%22%2F%3E%3Cpath%20d%3D%22m1%205.98873%201.11724%205.36447-.71451.5313%201.06527.8034-.80545.622%201.05228.9588-.66255.4795%201.51997%201.7752%206.89835-2.0085%204.8846-4.1205-12.69233-9.3944z%22%20fill%3D%22%23763e1a%22%20stroke%3D%22%23763e1a%22%2F%3E%3Cpath%20d%3D%22m32.0489%2016.5234-6.8983-2.0085%202.0786%203.1358-3.1049%206.0513%204.1052-.0519h6.1318z%22%20fill%3D%22%23f5841f%22%20stroke%3D%22%23f5841f%22%2F%3E%3Cpath%20d%3D%22m10.4705%2014.5149-6.89828%202.0085-2.29944%207.1267h6.11883l4.10519.0519-3.10487-6.0513z%22%20fill%3D%22%23f5841f%22%20stroke%3D%22%23f5841f%22%2F%3E%3Cpath%20d%3D%22m19.8241%2017.9876.4417-7.5932%202.0007-5.4034h-8.9119l2.0006%205.4034.4417%207.5932.1689%202.3842.013%205.8958h3.6635l.013-5.8958z%22%20fill%3D%22%23f5841f%22%20stroke%3D%22%23f5841f%22%2F%3E%3C%2Fg%3E%3C%2Fsvg%3E"' }
+                    p6963s.set(info.uuid, { provider, info })
+                    gate.resolve()
                 }, 0)
-            } else mGate.resolve(false)
-            if (await mGate.promise) return
-
-            // # check to see if trustwallet is lurking somewhere
-            if (p1193) {
-                // is it the 1193?
-                if ('isTrust' in p1193) {
-                    const result = this.operator.set(p1193)
-                    if (!(result instanceof Error)) {
-                        p1193.on('chainChanged', () => vortex.flow('chain'))
-                        p1193.on('accountsChanged', () => vortex.flow('account'))
-                        return
-                    }
-                }
-                // if our 1193 is a 5749, check if any of the providers there are trustwallet
-                if ('providers' in p1193 && p1193.providers instanceof Array && p1193.providers.find(p => 'isTrust' in p)) {
-                    const provider = p1193.providers.find(p => 'isTrust' in p)
-                    const result = this.operator.set(provider)
-                    if (!(result instanceof Error)) {
-                        provider.on('chainChanged', () => vortex.flow('chain'))
-                        provider.on('accountsChanged', () => vortex.flow('account'))
-                        return
-                    }
-                }
-                // check window.trustwallet
-                if  (g1193.trustwallet) {
-                    const provider = g1193.trustwallet
-                    const result = this.operator.set(provider)
-                    if (!(result instanceof Error)) {
-                        provider.on('chainChanged', () => vortex.flow('chain'))
-                        provider.on('accountsChanged', () => vortex.flow('account'))
-                        return
-                    }
-                }
-            }
-            
-            // # then try to get the first 6963 provider that responds within 250ms
-            // set up a gate
-            const gate = new Gate<P1193>()
-            // set up a 250ms second timeout
-            setTimeout(() => gate.reject(new Error('no eip-6963 provider request responses')), 250)
-            // listen for 6963 announcements
-            globalThis.addEventListener('eip6963:announceProvider', (event:EIP6963AnnounceProviderEvent) => {
-                // resolve gate with the first provider that repsonds
-                gate.resolve(event.detail.provider)
-            })
-            // request 6963 providers
-            globalThis.dispatchEvent(new Event('eip6963:requestProvider'))
-            // wait for a provider response or 250ms, whichever comes first
-            const provider = await gate.promise.catch(reason => new Error(reason))
-            // otherwise, pick the first provider in the list
-            if (!(provider instanceof Error)) {
-                // try to set them
-                const result = this.operator.set(provider)
-                if (!(result instanceof Error)) {
-                    provider.on('chainChanged', () => vortex.flow('chain'))
-                    provider.on('accountsChanged', () => vortex.flow('account'))
-                    return
-                }
+                await gate.promise
             }
 
-            // # then try to get the 1193 provider
-            if (p1193) {
-                const result = this.operator.set(p1193)
-                if (!(result instanceof Error)) {
-                    p1193.on('chainChanged', () => vortex.flow('chain'))
-                    p1193.on('accountsChanged', () => vortex.flow('account'))
-                    return
-                }
-            }
-
-            // # if we end up here, alert that no wallet was found
-            alert('no wallet found')
-            this.operator.set(new Error('no wallet found'))
+            // something like this.operator.set(await connector.choose(providers)))
+            // if (providers.size > 1) this.operator.set(await Connector
+            if (p6963s.size > 1) this.operator.set(await choose([...p6963s.values()]))
+            else if (p6963s.size == 1) this.operator.set((p6963s.values().next().value as P6963).provider)
+            else if (g1193.ethereum) this.operator.set(g1193.ethereum)
+            else { alert('no web3 providers detected'); this.operator.set(new Error('no web3 providers detected')) }
 
         },
         schema: sp1193
@@ -289,6 +245,7 @@ const data = {
             const p1193 = this.operator.get('p1193') as P1193
             const error = this.operator.errors(this.dependencies)[0] as undefined|Error
             if (error) { this.operator.set(error); return }
+            console.log('requesting chain')
             this.operator.set(
                 await p1193.request({ method: 'eth_chainId', params: [] })
                     .then(z.string().transform(BigInt).parseAsync)
@@ -305,8 +262,14 @@ const data = {
             const p1193 = this.operator.get('p1193') as P1193
             const error = this.operator.errors(this.dependencies)[0] as undefined|Error
             if (error) { this.operator.set(error); return }
+            if (this.flow == 'init') console.log('requesting accounts')
             const addresses = this.flow == 'init'
-                ? await p1193.request({ method: 'eth_requestAccounts', params: [] }).catch(reason => new Error(String(reason))) as Error|string[]
+                ? await p1193.request({ method: 'eth_requestAccounts', params: [] }).catch(reason => {
+                    this.operator.controller.abort()
+                    for (const key in this.operator.tState) this.operator.tState[key] = undefined
+                    vortex.updaters.value = new Set()
+                    return new Error(JSON.stringify(reason))
+                }) as string[]
                 : await p1193.request({ method: 'eth_accounts', params: [] }).catch(reason => new Error(String(reason))) as Error|string[]
             this.operator.set(
                 addresses instanceof Error
