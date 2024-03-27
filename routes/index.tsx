48e977bceb55f123edae2d51f70c33d526d3ee5e
Corvardt <124924375+corvardt@users.noreply.github.com>
2024-03-27 23:30:23 +0100

    Update index.tsx

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="w-full h-screen flex items-center justify-center">
+    <div className="flex w-full grow flex-col items-center justify-center">
       <a className="text-black dark:text-gray-200 font-[Poppins] text-4xl hover:scale-105" href="/bridge">Vertigo</a>
     </div>
   );
