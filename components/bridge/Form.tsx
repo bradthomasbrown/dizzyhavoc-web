e44cc9b8fdba6b9669971d53acbd437dbf720214
Brad Brown <bradbrown@bradbrown.llc>
2024-03-27 15:40:10 -0400

    new Form component for bridge

---
+++ components/bridge/Form.tsx
@@ -0,0 +1,24 @@
+import { UI } from '../../islands/bridge/UI.tsx'
+
+export function Form() {
+    return (
+      <>
+        {/* app form */}
+        <div className="relative sm:w-[500px] w-[360px] bg-blur2 shadow-xl rounded-xl flex flex-col justify-center items-center min-h-[25rem] overflow-hidden">
+            
+          {/* absolute container for background image */}
+          <div class="z-[-1] w-full h-full absolute flex justify-center items-center">
+            <image
+              class="static w-96 h-96 select-none pointer-events-none scale-150 blur-sm opacity-10 dark:brightness-50 brightness-150 rotate-[-12deg]"
+              src="/misc/dzhv-art-blue-yellow-busy.jpg"
+            />
+          </div>
+  
+          {/* bridge UI */}
+          <UI />
+
+        </div>
+      </>
+    );
+  }
+  
