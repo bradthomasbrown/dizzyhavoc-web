271e1d93e822bf968bb8ce1cd9c57db7be63f50d
Brad Brown <bradbrown@bradbrown.llc>
2024-03-26 20:50:30 -0400

    slight left pad to app form title

--- routes/faucet.tsx
+++ routes/faucet.tsx
@@ -7,7 +7,7 @@ export default function Faucet() {
         {/*grow to keep footer at bottom, includes title and app form*/}
         <div class="flex w-full h-screen flex-col items-center mb-[5rem]">
           <div className="min-w-[360px] flex-col sm:min-w-[500px] bg-blur2 shadow-xl rounded-xl flex min-h-[25rem] my-auto">
-            <div class="lg:text-[1.8rem] unselectable text-[1.5rem] font-[Poppins] font-medium dark:text-[#d2d2d2] text-[#282828]">
+            <div class="lg:text-[1.8rem] unselectable text-[1.5rem] font-[Poppins] font-medium dark:text-[#d2d2d2] text-[#282828] pl-2">
               testnet faucet
             </div>
             <Form />
