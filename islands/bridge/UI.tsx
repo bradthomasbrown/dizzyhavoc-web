420121e43105b9fcc4db837540249689b7a71a5f
Brad Brown <bradbrown@bradbrown.llc>
2024-03-28 22:15:31 -0400

    form formatting changes

--- islands/bridge/UI.tsx
+++ islands/bridge/UI.tsx
@@ -88,102 +88,107 @@ export function UI() {
           <>
             <ConnectionInfo />
 
-            <svg
-              onClick={flipChosen}
-              class="z-10 absolute x-[50%] y-[50%] [hover:scale-[105%] active:scale-[95%] cursor-pointer w-8 h-8 text-[#282828] dark:text-[#d2d2d2]"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              fill="none"
-              viewBox="0 0 24 24"
-            >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M17.651 7.65a7.131 7.131 0 0 0-12.68 3.15M18.001 4v4h-4m-7.652 8.35a7.13 7.13 0 0 0 12.68-3.15M6 20v-4h4"
-              />
-            </svg>
+            <div class="w-full sm:px-16 px-8">
 
-            <div>
-
-              <div class="bg-blur2 shadow-xl w-auto flex flex-col">
+              <div class="p-2 bg-blur2 shadow-xl w-auto flex flex-col font-[Poppins] text-[#282828] dark:text-[#d2d2d2]">
 
                 <div class="flex">
                   <div class="grow">gas</div>
                   <div>time</div>
                 </div>
 
-                <hr/>
+                <div class="relative">
 
-                <div class="flex flex-col">
-                  <div class="flex p-1">
-                    <div class="grow font-[Poppins] text-[#282828] dark:text-[#d2d2d2] text-sm">
-                      Burn
-                    </div>
-                    <div class="font-[Poppins] text-[#282828] dark:text-[#d2d2d2] text-sm">
-                      From
-                    </div>
-                  </div>
-                  <div class="flex">
-                    <div class="grow">amount1</div>
-                    <FhChainPicker
-                      chosen={chosenChains}
-                      which={"from"}
-                      onClick={pickChain}
-                      addClass="translate-x-[50%]"
+                  <svg
+                    onClick={flipChosen}
+                    class="z-10 absolute top-[50%] left-[50%] translate-y-[-50%] translate-x-[-50%] [hover:scale-[105%] active:scale-[95%] cursor-pointer w-8 h-8 text-[#282828] dark:text-[#d2d2d2]"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M17.651 7.65a7.131 7.131 0 0 0-12.68 3.15M18.001 4v4h-4m-7.652 8.35a7.13 7.13 0 0 0 12.68-3.15M6 20v-4h4"
                     />
-                  </div>
-                  <div class="flex">
-                    <div class="grow">amount2</div>
-                    <div class="font-[Poppins] text-[#282828] dark:text-[#d2d2d2] font-extralight text-sm">
-                      {chosenChains.value["from"]?.shortName ?? ""}
+                  </svg>
+
+                  <div class="flex flex-col">
+                    <div class="flex">
+                      <div class="grow text-sm">
+                        Burn
+                      </div>
+                      <div class="text-sm">
+                        From
+                      </div>
+                    </div>
+                    <div class="flex">
+                      <input class="w-0 grow flex font-mono items-center text-[32px] bg-transparent" placeholder="0"/>
+                      <FhChainPicker
+                        chosen={chosenChains}
+                        which={"from"}
+                        onClick={pickChain}
+                        addClass="translate-x-[50%]"
+                      />
+                    </div>
+                    <div class="flex">
+                      <div class="grow font-extralight text-sm font-mono">
+                        $123.00
+                      </div>
+                      <div class="font-extralight text-sm">
+                        {chosenChains.value["from"]?.shortName ?? ""}
+                      </div>
                     </div>
                   </div>
-                </div>
 
-                <div class="flex">
-                  <div class="grow border-t border-white" />
-                  <div class="w-8" />
-                  <div class="grow border-t border-white" />
-                </div>
-
-                <div class="flex flex-col">
-                  <div class="flex p-1">
-                    <div class="grow font-[Poppins] text-[#282828] dark:text-[#d2d2d2] text-sm">
-                      Mint
-                    </div>
-                    <div class="font-[Poppins] text-[#282828] dark:text-[#d2d2d2] text-sm">
-                      To
-                    </div>
-                  </div>
                   <div class="flex">
-                    <div class="grow">amount1</div>
-                    <FhChainPicker
-                      chosen={chosenChains}
-                      which={"to"}
-                      onClick={pickChain}
-                      addClass="translate-x-[50%]"
-                    />
+                    <div class="grow border-t border-white" />
+                    <div class="w-8" />
+                    <div class="grow border-t border-white" />
                   </div>
-                  <div class="flex">
-                    <div class="grow">amount2</div>
-                    <div>{chosenChains.value["to"]?.shortName ?? ""}</div>
-                  </div>
-                </div>
 
-                <hr/>
+                  <div class="flex flex-col">
+                    <div class="flex">
+                      <div class="grow text-sm">
+                        Mint
+                      </div>
+                      <div class="text-sm">
+                        To
+                      </div>
+                    </div>
+                    <div class="flex">
+                      <input class="w-0 grow flex font-mono items-center text-[32px] bg-transparent" placeholder="0"/>
+                      <FhChainPicker
+                        chosen={chosenChains}
+                        which={"to"}
+                        onClick={pickChain}
+                        addClass="translate-x-[50%]"
+                      />
+                    </div>
+                    <div class="flex">
+                    <div class="grow font-extralight text-sm font-mono">
+                        $99.00
+                      </div>
+                      <div>{chosenChains.value["to"]?.shortName ?? ""}</div>
+                    </div>
+                  </div>
+
+                </div>
 
               </div>
 
               <Button
-                addClass="text-[#3d3d3d] dark:text-[#ccb286]"
+                addClass="relative text-[#3d3d3d] dark:text-[#ccb286] z-10"
                 disabled={false}
                 onClick={false ? () => {} : bridge}
                 rounding="rounded-b-lg"
                 wiggle={false}
+                width="100%"
               >
                 Bridge
               </Button>
