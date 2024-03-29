e8abef2a0192f10b1f832c2366145a6d03fe89e2
Brad Brown <bradbrown@bradbrown.llc>
2024-03-28 20:32:12 -0400

    implementing fabian layout

--- islands/bridge/UI.tsx
+++ islands/bridge/UI.tsx
@@ -94,6 +94,67 @@ export function UI() {
           <>
             <ConnectionInfo />
 
+            <svg onClick={flipChosen} class="z-10 absolute x-[50%] y-[50%] [hover:scale-[105%] active:scale-[95%] cursor-pointer w-8 h-8 text-[#282828] dark:text-[#d2d2d2]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.651 7.65a7.131 7.131 0 0 0-12.68 3.15M18.001 4v4h-4m-7.652 8.35a7.13 7.13 0 0 0 12.68-3.15M6 20v-4h4"/>
+            </svg>
+
+            <div class="bg-blur2 shadow-xl w-[400px] flex flex-col">
+
+              <div class="flex">
+                <div class="grow">gas</div>
+                <div>time</div>
+              </div>
+
+              <div class="flex flex-col">
+                <div class="flex">
+                  <div class="grow">burn</div>
+                  <div>from</div>
+                </div>
+                <div class="flex">
+                  <div class="grow">amount1</div>
+                  <FhChainPicker
+                    chosen={chosenChains}
+                    which={"from"}
+                    onClick={pickChain}
+                    addClass="translate-x-[50%]"
+                  />
+                </div>
+                <div class="flex">
+                  <div class="grow">amount2</div>
+                  <div>name</div>
+                </div>
+              </div>
+
+              <div class="flex">
+                <div class="grow border-t border-white"/>
+                <div class="w-8"/>
+                <div class="grow border-t border-white"/>
+              </div>
+
+              <div class="flex flex-col">
+                <div class="flex">
+                  <div class="grow">mint</div>
+                  <div>to</div>
+                </div>
+                <div class="flex">
+                  <div class="grow">amount1</div>
+                  <FhChainPicker
+                    chosen={chosenChains}
+                    which={"to"}
+                    onClick={pickChain}
+                    addClass="translate-x-[50%]"
+                  />
+                </div>
+                <div class="flex">
+                  <div class="grow">amount2</div>
+                  <div>name</div>
+                </div>
+              </div>
+
+              <div class="flex justify-center">bridge</div>
+              
+            </div>
+
             {/* balance */}
             {/* <Balance/> */}
 
@@ -105,14 +166,13 @@ export function UI() {
             {/* <ListInput list="addrs" placeholder="receiving address" onInput={e => recipient.value = e.currentTarget.value}/> */}
 
             {/* 'from -> to' row */}
-            <div class="flex flex-row gap-x-4">
+            {/* <div class="flex flex-row gap-x-4">
               <FhChainPicker
                 chosen={chosenChains}
                 which={"from"}
                 onClick={pickChain}
               />
 
-              {/* swapper */}
               <svg onClick={flipChosen} class="hover:scale-[102%] active:scale-[98%] cursor-pointer w-8 h-8 text-[#282828] dark:text-[#d2d2d2]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.651 7.65a7.131 7.131 0 0 0-12.68 3.15M18.001 4v4h-4m-7.652 8.35a7.13 7.13 0 0 0 12.68-3.15M6 20v-4h4"/>
               </svg>
@@ -122,7 +182,7 @@ export function UI() {
                 which={"to"}
                 onClick={pickChain}
               />
-            </div>
+            </div> */}
 
             {/* bridge button */}
             {
