45a6939624792bb5fa2b979eb9c6f5baca56d65b
Brad Brown <bradbrown@bradbrown.llc>
2024-03-31 03:31:58 -0400

    isolate Divider

---
+++ components/common/Divider.tsx
@@ -0,0 +1,9 @@
+export function Divider() {
+  return (
+    <div class="flex">
+      <div class="grow border-t border-white" />
+      <div class="w-8" />
+      <div class="grow border-t border-white" />
+    </div>
+  )
+}
