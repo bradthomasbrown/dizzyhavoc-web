68c6352f0832eaf21b4ead4d424e5a35b7b0d45e
Brad Brown <bradbrown@bradbrown.llc>
2024-03-31 03:41:57 -0400

    isolate ActionButton

---
+++ islands/bridge/ActionButton.tsx
@@ -0,0 +1,22 @@
+import { Button } from "../../components/common/Button.tsx";
+import { bridge } from "../../lib/utils/bridge.ts";
+import { Connector, status } from "../common/Connector.tsx";
+
+export function ActionButton() {
+
+  if (status.value == 'Connected') {
+    return <Button
+      addClass="relative text-[#3d3d3d] dark:text-[#ccb286] z-10"
+      disabled={false}
+      onClick={() => bridge()}
+      rounding="rounded-b-lg"
+      wiggle={false}
+      width="100%"
+    >
+      Bridge
+    </Button>
+  }
+
+  return <Connector/>
+  
+}
