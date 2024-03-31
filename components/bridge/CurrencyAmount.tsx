70270abf9d18fded1c267f4f5f2cbbce8f44f463
Brad Brown <bradbrown@bradbrown.llc>
2024-03-31 17:46:40 -0400

    add more details, like label, chain name, $ amount

--- components/bridge/CurrencyAmount.tsx
+++ components/bridge/CurrencyAmount.tsx
@@ -1,18 +1,21 @@
-import { Input } from "../../islands/common/Input.tsx"
+import { CurrencyAmountInput } from './CurrencyAmountInput.tsx'
 import { FhChainPicker } from '../../islands/common/FhChainPicker.tsx'
 import { chosenChains } from '../../lib/bridge/chosenChains.ts'
-import { pickChain } from '../../lib/bridge/pickChain.tsx'
+import { pickChain as onClick } from '../../lib/bridge/pickChain.tsx'
+import { chainAbrv } from '../../lib/chainAbrv.ts'
 
-export function CurrencyAmount(props:{ direction:string }) {
+export function CurrencyAmount({ type:id, label }:{ type:string, label?:string }) {
+  const chainId = chosenChains.value[id]?.chainId
   return (
-    <>
-      <Input/>
-      <FhChainPicker
-        chosen={chosenChains}
-        id={props.direction}
-        onClick={pickChain}
-        addClass="translate-x-[calc(50%+8px)]"
-      />
-    </>
+    <div class="grid grid-rows-[auto,1fr] grid-cols-2">
+      <div>{label}</div>
+      <div class="text-right">{id}</div>
+      <div class="flex col-span-2">
+        <CurrencyAmountInput/>
+        <FhChainPicker {...{ chosenChains, id, onClick }}/>
+      </div>
+      <div>$0.00</div>
+      <div class="text-right">{chainAbrv(chainId)}</div>
+    </div>
   )
 }
