4e9c478cba084a5e18627371992da4d0c3d7de21
Brad Brown <bradbrown@bradbrown.llc>
2024-03-31 16:28:06 -0400

    isolate Body component

---
+++ components/bridge/Body.tsx
@@ -0,0 +1,25 @@
+import { ArtContainer } from "./ArtContainer.tsx";
+import { ActiveForm } from '../../islands/bridge/ActiveForm.tsx'
+
+/**
+ * The Body of the app where art and interactables are usually located 
+ */
+export function Body() {
+  return (
+    <div className={`
+      relative
+      sm:w-[500px] w-[360px]
+      bg-blur2
+      shadow-xl
+      rounded-xl
+      flex flex-col 
+      justify-center
+      items-center
+      overflow-hidden
+      min-h-[25rem] max-h-[500px] h-[500px]`
+    }>
+      <ArtContainer/>
+      <ActiveForm/>
+    </div>
+  );
+}
