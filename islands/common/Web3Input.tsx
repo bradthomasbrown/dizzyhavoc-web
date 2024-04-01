440363c1e197ff85ac043f092324662ad30b36a8
Corvardt <124924375+corvardt@users.noreply.github.com>
2024-04-01 15:04:34 +0200

    Update Web3Input.tsx

--- islands/common/Web3Input.tsx
+++ islands/common/Web3Input.tsx
@@ -76,7 +76,7 @@ export function Web3Input(
   return (
     <div class="flex flex-col mr-4">
        <input
-         type="decimals"
+         type="number"
          class="max-w-[200px] px-2 text-[32px] font-[monospace] bg-transparent [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
          {...{ disabled }}
          value={numberValue}
