ae341a4fce2e1ea0888a2ecca3195e76b02c465c
Brad Brown <bradbrown@bradbrown.llc>
2024-04-01 03:01:23 -0400

    get rid of old decimated attribs and change amounts import

--- components/bridge/CurrencyAmountInput.tsx
+++ components/bridge/CurrencyAmountInput.tsx
@@ -1,5 +1,5 @@
 import { Web3Input } from "../../islands/common/Web3Input.tsx";
-import { amounts } from "./CurrencyPair.tsx";
+import { amounts } from "../../lib/bridge/amounts.ts";
 
 export function CurrencyAmountInput({ id, disabled }: { id: string, disabled?: boolean }) {
   return (
@@ -7,23 +7,6 @@ export function CurrencyAmountInput({ id, disabled }: { id: string, disabled?: b
       maxVal={10n ** 9n * 10n ** 18n}
       decimals={18n}
       {...{ amounts, id, disabled }}
-      // amounts={amounts}
-      // id={props.id}
-      // decimals={18n}
-      // disabled={props.id == "to" ? true : false}
-      // autoComplete="off"
-      // type="number" // this handles most validation for us
-      // placeholder="0"
-      // class={`
-      //   [&::-webkit-outer-spin-button]:appearance-none
-      //   [&::-webkit-inner-spin-button]:appearance-none
-      //   w-100
-      //   font-mono
-      //   text-[32px]
-      //   bg-transparent
-      //   px-2
-      //   mr-2
-      //   max-w-[200px]`}
     />
   );
 }
