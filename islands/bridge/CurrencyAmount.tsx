9e07502b7e54c1869e78d0fa6e3232799ee65a69
Brad Brown <bradbrown@bradbrown.llc>
2024-04-01 02:11:27 -0400

    lots of small rewrites, styles and props mostly

--- islands/bridge/CurrencyAmount.tsx
+++ islands/bridge/CurrencyAmount.tsx
@@ -7,17 +7,19 @@ import { extVortex } from "../../lib/bridge/extVortex/extVortex.ts";
 import { IS_BROWSER } from "$fresh/runtime.ts";
 import { computed, Signal } from "@preact/signals";
 import { chainToPrice } from "../../lib/bridge/chainToPrice.ts";
-import { amounts } from "./CurrencyPair.tsx";
+import { amounts } from "../../components/bridge/CurrencyPair.tsx";
 
 chosenChains.subscribe(() => extVortex.flow("priceCheck"));
 
 export function CurrencyAmount(
-  { type: id, label, addClass }: {
-    type: string;
+  { id, label, disabled, order }: {
+    id: string;
     label?: string;
-    addClass?: string;
+    order?: string;
+    disabled?: boolean
   },
 ) {
+
   const usdDisplayValue = computed<undefined | string>(() => {
     if (!IS_BROWSER) return undefined;
     const chain = chosenChains.value[id];
@@ -25,24 +27,27 @@ export function CurrencyAmount(
     if (!chain || !dsData) return undefined;
     const price = chainToPrice(chain, dsData);
     if (!price) return undefined;
-    const amount = amounts.value[id] ?? 0;
+    const amount = amounts.value.get(id) ?? 0n;
     if (amount === undefined) return undefined;
-    return (price * amount).toLocaleString("en-US", {
+    return (price * parseFloat(String(amount)) / 1e18).toLocaleString("en-US", {
       style: "currency",
       currency: "USD",
     });
   });
+
   const chainId = chosenChains.value[id]?.chainId;
+
   return (
-    <div class={`grid grid-rows-[auto,1fr] grid-cols-2 ${addClass}`}>
-      <div>{label}</div>
-      <div class="text-right">{id}</div>
+    <div class={`grid grid-rows-[auto,1fr] grid-cols-2 ${order}`}>
+      <div class="brightness-90">{label}</div>
+      <div class="text-right brightness-90">{id}</div>
       <div class="flex col-span-2">
-        <CurrencyAmountInput {...{ id }} />
+        <CurrencyAmountInput {...{ id, disabled }} />
         <FhChainPicker {...{ chosenChains, id, onClick }} />
       </div>
-      <div>{usdDisplayValue.value ?? <>&nbsp;</>}</div>
-      <div class="text-right">{chainAbrv(chainId)}</div>
+      <div class="font-mono text-sm font-thin brightness-75">{usdDisplayValue.value ?? <>&nbsp;</>}</div>
+      <div class="text-right font-mono text-sm font-thin brightness-75">{chainAbrv(chainId)}</div>
     </div>
   );
+
 }
