f9dccbc4ed76ff2310d2ea049613290c411a29e0
Brad Brown <bradbrown@bradbrown.llc>
2024-04-01 02:11:45 -0400

    i think deprecated

--- components/bridge/CurrencyAmountInput.tsx
+++ components/bridge/CurrencyAmountInput.tsx
@@ -1,33 +1,29 @@
-import { amounts } from "../../islands/bridge/CurrencyPair.tsx";
-import { JSX } from "preact/jsx-runtime";
+import { Web3Input } from "../../islands/common/Web3Input.tsx";
+import { amounts } from "./CurrencyPair.tsx";
 
-export function CurrencyAmountInput(props: { id: string }) {
-  function onInput(e: JSX.TargetedEvent<HTMLInputElement>) {
-    amounts.value = {
-      ...amounts.value,
-      [props.id]: Number(e.currentTarget.value),
-    };
-  }
-
+export function CurrencyAmountInput({ id, disabled }: { id: string, disabled?: boolean }) {
   return (
-    <input
-      disabled={props.id == "to" ? true : false}
-      value={amounts.value[props.id]}
-      onInput={onInput}
-      autoComplete="off"
-      type="number" // this handles most validation for us
-      placeholder="0"
-      class={`
-        [&::-webkit-outer-spin-button]:appearance-none
-        [&::-webkit-inner-spin-button]:appearance-none
-        w-100
-        font-mono
-        text-[32px]
-        bg-transparent
-        px-2
-        mr-2
-        max-w-[200px]
-      `}
+    <Web3Input
+      maxVal={10n ** 9n * 10n ** 18n}
+      decimals={18n}
+      {...{ amounts, id, disabled }}
+      // amounts={amounts}
+      // id={props.id}
+      // decimals={18n}
+      // disabled={props.id == "to" ? true : false}
+      // autoComplete="off"
+      // type="number" // this handles most validation for us
+      // placeholder="0"
+      // class={`
+      //   [&::-webkit-outer-spin-button]:appearance-none
+      //   [&::-webkit-inner-spin-button]:appearance-none
+      //   w-100
+      //   font-mono
+      //   text-[32px]
+      //   bg-transparent
+      //   px-2
+      //   mr-2
+      //   max-w-[200px]`}
     />
   );
 }
