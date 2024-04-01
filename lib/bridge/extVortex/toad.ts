f3759e2f54d7479cc5b75d1d8f9cb21a5458149d
Brad Brown <bradbrown@bradbrown.llc>
2024-03-31 21:41:10 -0400

    increase toad delay to 5s from 250ms

--- lib/bridge/extVortex/toad.ts
+++ lib/bridge/extVortex/toad.ts
@@ -1,3 +1,3 @@
-import { Toad } from "https://cdn.jsdelivr.net/gh/bradbrown-llc/toad@0.0.3/mod.ts";
+import { Toad } from "https://cdn.jsdelivr.net/gh/bradbrown-llc/toad@0.0.6/mod.ts";
 
-export const toad = new Toad(250);
+export const toad = new Toad(5000);
