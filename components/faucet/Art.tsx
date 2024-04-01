b3b5ab933e7bbcb439ee29187463b2d99a546ff0
Brad Brown <bradbrown@bradbrown.llc>
2024-04-01 16:16:58 -0400

    put faucet art back to normal

--- components/faucet/Art.tsx
+++ components/faucet/Art.tsx
@@ -9,15 +9,14 @@ export function Art() {
           select-none
           pointer-events-none
           scale-[1.50]
-          blur-[4px]
+          blur-sm
           opacity-10
           dark:brightness-50
-          contrast-150
           brightness-150
-          rotate-[45deg]
-          translate-x-8 
-          translate-y-[-24px]`}
+          rotate-[-12deg]`}
       src="/misc/dzhv-art-blue-yellow-busy.jpg"
     />
   );
 }
+
+// static w-96 h-96 select-none pointer-events-none scale-150 blur-sm opacity-10 dark:brightness-50 brightness-150 rotate-[-12deg]
