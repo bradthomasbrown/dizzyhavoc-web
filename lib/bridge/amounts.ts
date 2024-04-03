acba3bbee6ba5bb939ed873a82c218400ae0d68f
Brad Brown <bradbrown@bradbrown.llc>
2024-04-02 20:30:14 -0400

    amounts overhaul (i don't think this needs to be a map)

--- lib/bridge/amounts.ts
+++ lib/bridge/amounts.ts
@@ -1,29 +1,33 @@
-import { effect, Signal } from "@preact/signals";
-import { extVortex } from "./extVortex/extVortex.ts";
+import { computed, Signal } from "@preact/signals";
+import { viVortex } from "./viVortex/viVortex.ts";
 import { chosenChains } from "./chosenChains.ts";
 
-export const amounts = new Map([
+const toChain = chosenChains.get('to')!
+const dummy = new Signal(0)
+toChain.subscribe(() => dummy.value++)
+
+export const amounts:Map<string,Signal<undefined|bigint>> = new Map([
   ['from', new Signal<undefined|bigint>(undefined)],
-  ['to', new Signal<undefined|bigint>(undefined)]
+  ['to', computed<undefined|bigint>(() => {
+
+    dummy.value
+
+    if (amounts.get('from')!.value === undefined) return
+
+    const econConfs = viVortex.uState.econConf.value
+    console.log(econConfs)
+    if (econConfs === undefined || econConfs instanceof Error) return
+    
+    const chain = toChain.value
+    if (chain === undefined) return
+    
+    const econConf = econConfs.get(chain)
+    if (!econConf) return
+
+    let toAmount = amounts.get('from')!.value! - BigInt(econConf.baseFee) * 10n ** 18n;
+    if (toAmount < 0n) toAmount = 0n;
+
+    return toAmount;
+
+  })]
 ])
-
-effect(() => {
-
-  const fromAmount = amounts.get('from')!.value;
-  if (fromAmount === undefined) return
-
-  const econConfs = extVortex.uState.econConf.value
-  if (econConfs === undefined || econConfs instanceof Error) return
-  
-  const chain = chosenChains.get('to')!.value
-  if (chain === undefined) return
-  
-  const econConf = econConfs.get(chain)
-  if (!econConf) return
-
-  let toAmount = fromAmount - BigInt(econConf.baseFee) * 10n ** 18n;
-  if (toAmount < 0n) toAmount = 0n;
-
-  amounts.get('to')!.value = toAmount;
-
-});
