98b3b8c3e5ee7c00de8a7f879f3327c81e225cf9
Brad Brown <bradbrown@bradbrown.llc>
2024-04-03 12:43:29 -0400

    activeChains key contains activeChains, not chains

--- lib/vertinfo/methods/getActiveChains.ts
+++ lib/vertinfo/methods/getActiveChains.ts
@@ -3,6 +3,6 @@ import { JsonRpcRequest } from "../types/mod.ts";
 import { kv } from "../kv.ts";
 
 export async function getActiveChains({ id }: Pick<JsonRpcRequest, "id">) {
-  const kvem = await kv.get<number[]>(["chains"]);
+  const kvem = await kv.get<number[]>(["activeChains"]);
   return jsonRpc.response({ result: kvem.value, id });
 }
