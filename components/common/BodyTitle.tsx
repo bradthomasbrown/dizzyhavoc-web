56b51494ae3d913bf0fa0f6c3c80dfaace09c193
Brad Brown <bradbrown@bradbrown.llc>
2024-04-06 05:44:57 -0400

    isolated bodyTitle with children

---
+++ components/common/BodyTitle.tsx
@@ -0,0 +1,14 @@
+export function BodyTitle(props: { text: string }) {
+  return (
+    <div
+      class={`
+        pl-2
+        unselectable
+        text-[1.5rem] lg:text-[1.8rem]
+        font-medium
+      `}
+    >
+      {props.text}
+    </div>
+  );
+}
