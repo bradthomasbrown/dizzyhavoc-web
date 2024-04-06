188825ba2da98ece17dabd9e731305b168206346
Brad Brown <bradbrown@bradbrown.llc>
2024-04-06 05:43:25 -0400

    compressed and simplified connector

--- islands/common/Connector.tsx
+++ islands/common/Connector.tsx
@@ -1,59 +1,38 @@
-import { Gate } from "https://cdn.jsdelivr.net/gh/bradbrown-llc/gate@0.0.1/mod.ts";
-import { batch, computed, Signal } from "@preact/signals";
+import { batch, Signal } from "@preact/signals";
 import { Button } from "../../components/common/Button.tsx";
-import { evmVortex } from "../../lib/faucet/evmVortex/evmVortex.ts";
-import { P1193, P6963 } from "../../lib/state2/providers.ts";
-import { Which } from "./which/Which.tsx";
+import { getProviders } from "lib";
 
-export const status = computed(() => {
-  const uAddresses = evmVortex.uState.addresses.value;
-  const updaters = evmVortex.updaters.value;
-  const updater = evmVortex.data.addresses.updater;
-  const tAddresses = evmVortex.tState.addresses;
-  const foo = `?${!uAddresses ? 0 : uAddresses instanceof Error ? 1 : 2}${
-    !updaters.has(updater) ? 0 : 2
-  }${!tAddresses ? 0 : tAddresses instanceof Error ? 1 : 2}`;
-  if (uAddresses && !(uAddresses instanceof Error)) return "Connected";
-  if (updaters.has(updater)) return "Connecting";
-  if ((tAddresses && !(tAddresses instanceof Error))) return "Loading";
-  if (
-    (!uAddresses || uAddresses instanceof Error) &&
-    (!tAddresses || tAddresses instanceof Error) &&
-    !updaters.has(updater)
-  ) return "Connect";
-  return foo;
-});
+const text = new Signal<string>("Connect")
+const textSize = new Signal<undefined|string>(undefined)
+const disabled = new Signal<boolean>(false)
 
-const disabled = computed(() => status.value != "Connect");
+export enum ConnectorState { READY, PICK, LOADING }
 
-const choices = new Signal<undefined | P6963[]>(undefined);
-const choiceGate = new Signal<undefined | Gate<P1193>>(undefined);
-
-export function onChoice(p6963: P6963) {
+function set(state:ConnectorState) {
   batch(() => {
-    choices.value = undefined;
-    choiceGate.value?.resolve(p6963.provider);
-    choiceGate.value = undefined;
-  });
+    switch (state) {
+      case ConnectorState.READY:
+        text.value = 'Connect'
+        textSize.value = undefined
+        disabled.value = false
+        break
+      case ConnectorState.PICK:
+        text.value = "Getting Providers"
+        textSize.value = "text-sm"
+        disabled.value = true
+        break
+      case ConnectorState.LOADING:
+        text.value = "Loading"
+        textSize.value = undefined
+        disabled.value = true
+    }
+  })
 }
 
-export const which = new Signal<undefined | ReturnType<typeof Which>>(
-  undefined,
-);
-
-function onConnect() {
-  evmVortex.flow("init");
-}
-
-export function Connector(props: { addClass?: string }) {
-  return (
-    <Button
-      disabled={false}
-      wiggle={true}
-      addClass={`text-[#3d3d3d] dark:text-[#d7d7d7] ${props.addClass}`}
-      onClick={onConnect}
-    >
-      {status.value}
-    </Button>
-  );
-}
+export const Connector = Object.assign(
+  function () {
+    function onClick() { set(ConnectorState.LOADING); getProviders() }
+    return <Button {...{ textSize, disabled, onClick }}>{text}</Button>
+  },
+  { set }
+)
