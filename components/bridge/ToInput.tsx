29b981e55acfadc88894a80a9ac856d97b9e569e
Brad Brown <bradbrown@bradbrown.llc>
2024-04-06 05:44:09 -0400

    isolated to input

---
+++ components/bridge/ToInput.tsx
@@ -0,0 +1,9 @@
+export function ToInput() {
+  return (
+    <input
+      class="max-w-[200px] px-2 text-[32px] font-[monospace] bg-transparent [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
+      disabled={true}
+      placeholder="foo"
+    />
+  );
+}
