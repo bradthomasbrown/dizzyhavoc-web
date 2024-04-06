aa03f46e9c0733817042665084067461277b4aa5
Brad Brown <bradbrown@bradbrown.llc>
2024-04-06 05:41:06 -0400

    add text color and font to app

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -17,13 +17,15 @@ export default function App({ Component }: PageProps) {
           url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap')
         </style>
       </head>
-      <body class="min-h-screen flex flex-col bg-[#ededed] dark:bg-[#191919]">
+      <body class="min-h-screen flex flex-col bg-[#ededed] dark:bg-[#191919] text-[#2c2c2c] dark:text-[#EAEAEA] font-[Poppins]">
         <NavBar />
         <Component />
         <Footer />
       </body>
-      {/* <script src="//cdn.jsdelivr.net/npm/eruda"></script>
-      <script>eruda.init();</script> */}
+      {
+        /* <script src="//cdn.jsdelivr.net/npm/eruda"></script>
+      <script>eruda.init();</script> */
+      }
     </html>
   );
 }
