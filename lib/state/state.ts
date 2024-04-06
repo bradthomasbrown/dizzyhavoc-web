d3bc9976965d3c011c950c4cc3dcb6af88fcdef6
Brad Brown <bradbrown@bradbrown.llc>
2024-04-06 05:40:11 -0400

    deprecated or moved

--- lib/state/state.ts
+++
@@ -1,9 +0,0 @@
-// import { rpc, UState, provider, addresses, chainId, dzhv, balance, dzhvBalance,
-//     height } from '../internal.ts'
-
-// // the user-displayed state, or UState
-// // take all state signals and add them to an object
-// const state:UState = { provider, addresses, rpc, chainId, dzhv, balance,
-//     dzhvBalance, height, stateNonce: 0n }
-
-// export { state }
--- lib/state/stateNonce.ts
+++
@@ -1,4 +0,0 @@
-import { signal } from "@preact/signals";
-import { DAppState, e } from "../internal.ts";
-
-const stateNonce = signal<bigint>(0n);
