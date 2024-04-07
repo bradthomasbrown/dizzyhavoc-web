d10b065435c1cd494e26a1eb29f007b59fc4b13e
Brad Brown <bradbrown@bradbrown.llc>
2024-04-07 17:27:42 -0400

    loading effect should be more subtle

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,8 +16,9 @@ export default {
     plugin(function ({ addUtilities }) {
       addUtilities({
         ".loading": {
+          border: "solid 1px #ffffff08",
           background:
-            "linear-gradient(45deg, rgba(0,0,0,0) 30%, rgba(128,255,255,0.125) 50%, rgba(0,0,0,0) 70%) no-repeat",
+            "linear-gradient(45deg, rgba(0,0,0,0) 30%, rgba(128,255,255,0.068) 50%, rgba(0,0,0,0) 70%) no-repeat",
           backgroundSize: "200% 200%",
           animation: "loading 2.5s ease infinite",
         },
