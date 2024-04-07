9327275ecd198bc82cc7bff6dfd095abfe9ceccd
Brad Brown <bradbrown@bradbrown.llc>
2024-04-06 20:00:58 -0400

    real funtions, not arrows

--- lib/bridge/chain.ts
+++ lib/bridge/chain.ts
@@ -4,14 +4,20 @@ import { dzkv } from "lib/mod.ts";
 import "lib/bridge/mod.ts";
 
 type T = Signal<null | Chain>;
-export const key = (id: string[]) => ["chains", ...id];
-export const ensure = (id: string[]) =>
+
+export function key(id: string[]) {
+  return ["chains", ...id];
+}
+
+export function ensure(id: string[]) {
   dzkv.ensure<T>(key(id), new Signal(null));
-export const get = (id: string[]) => {
+}
+
+export function get(id: string[]) {
   ensure(id);
   return dzkv.get<T>(key(id))!;
 };
-export const set = (id: string[], chain: Chain) => {
-  ensure(id);
+
+export function set(id: string[], chain: Chain) {
   get(id).value = chain;
 };
