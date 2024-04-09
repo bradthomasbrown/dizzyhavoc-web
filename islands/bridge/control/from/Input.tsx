e38fcbb51449228c1e812caa40d2fff66fb0b3e1
Brad Brown <bradbrown@bradbrown.llc>
2024-04-09 10:04:44 -0400

    input component

--- islands/bridge/control/from/Input.tsx
+++ islands/bridge/control/from/Input.tsx
@@ -1,50 +1,66 @@
-import { Signal, effect } from "@preact/signals";
-import { data, btos } from 'lib/bridge/mod.ts'
+import { effect, Signal } from "@preact/signals";
+import { btos, data, stob } from "lib/bridge/mod.ts";
 import { JSX } from "preact/jsx-runtime";
+import * as slip from "islands/bridge/control/from/Slip.tsx";
+import * as percents from "islands/bridge/control/from/Percents.tsx";
 
-const control = data.control.from
+// const control = data.control.from
 
-const disp = new Signal<null|bigint>(null)
+// const disp = new Signal<null|bigint>(null)
 
-effect(() => {
-  const addresses = data.addresses.get().value
-  const address = addresses.at(0)
-  const chain = data.chain.get(['from']).value
-  if (!address || !chain) return
-  const balance = data.balance.get(chain, address, ['dzhv']).f.value
-  if (balance === null) return
-  if (!(control.type.get().value == 'percent')) return
-  disp.value = balance * BigInt(control.percent.get().value) / 100n
-})
+// effect(() => {
+//   const addresses = data.addresses.get().value
+//   const address = addresses.at(0)
+//   const chain = data.chain.get(['from']).value
+//   if (!address || !chain) return
+//   const balance = data.balance.get(chain, address, ['dzhv']).f.value
+//   if (balance === null) return
+//   if (control.type.get().value == 'percent') {
+//     const input = balance * BigInt(control.percent.get().value) / 100n
+//     disp.value = input
+//     control.input.set(input)
+//   }
+//   if (control.type.get().value == 'input') {
+//     const input = control.input.get().value
+//     if (input === null) return
+//     disp.value = input
+//   }
+// })
 
-function onInput(e:JSX.TargetedEvent<HTMLInputElement>) {
-  control.type.set('number')
-  control.input.set(e.currentTarget.value)
+function onInput(e: JSX.TargetedEvent<HTMLInputElement>) {
+  // slip.signal.value = 0
+  // percents.deactivate()
+  // const input = stob(e.currentTarget.value, 18)
+  // control.input.set(input)
+  // disp.value = input
 }
 
 // https://i.kym-cdn.com/entries/icons/facebook/000/026/008/Screen_Shot_2018-04-25_at_12.24.22_PM.jpg
-function onKeyDown(e:JSX.TargetedKeyboardEvent<HTMLInputElement>) {
-  if (e.code == 'KeyE') e.preventDefault()
+function onKeyDown(e: JSX.TargetedKeyboardEvent<HTMLInputElement>) {
+  if (e.code == "KeyE") e.preventDefault();
 }
 
-export function Input() {
-  return (
-    <input
-      type="number"
-      class={`
-        row-start-2 col-start-1 col-span-1
-        w-48 h-12
-        px-2
-        [&::-webkit-outer-spin-button]:appearance-none
-        [&::-webkit-inner-spin-button]:appearance-none
-        text-3xl
-        font-mono
-        bg-transparent
-      `}
-      placeholder="0"
-      value={disp.value ? btos(disp.value, 18) : ''}
-      onKeyDown={onKeyDown}
-      onInput={onInput}
-    />
-  )
-}
+const display = new Signal("");
+export const Input = Object.assign(
+  function () {
+    return (
+      <input
+        type="number"
+        class={`
+          row-start-2 col-start-1 col-span-1
+          w-32 h-10
+          px-2
+          [&::-webkit-outer-spin-button]:appearance-none
+          [&::-webkit-inner-spin-button]:appearance-none
+          text-xl
+          font-mono
+          bg-transparent
+        `}
+        placeholder="0"
+        value={display}
+        onKeyDown={onKeyDown}
+        onInput={onInput}
+      />
+    );
+  },
+);
