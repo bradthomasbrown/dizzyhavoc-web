9270b6d1f54befce3c4c0a560607238e5e2be400
Brad Brown <bradbrown@bradbrown.llc>
2024-04-09 10:04:57 -0400

    new component concept, Control.From

--- islands/bridge/control/from/mod.tsx
+++ islands/bridge/control/from/mod.tsx
@@ -1,34 +1,28 @@
-import { Balance } from "islands/bridge/control/from/Balance.tsx"
-import { Input } from "islands/bridge/control/from/Input.tsx"
-import { Slip } from "islands/bridge/control/from/Slip.tsx"
+import { Balance } from "islands/bridge/control/from/Balance.tsx";
+import { Input } from "islands/bridge/control/from/Input.tsx";
+import { Percents } from "islands/bridge/control/from/Percents.tsx";
+import { Dollars } from "islands/bridge/control/from/Dollars.tsx";
 
-export const From = Object.assign(
-  function () {
-    return (
-      <div class="grid grid-rows-[auto,1fr] grid-cols-[auto,1fr]">
-        <Balance />
-        <Input/>
-        <Slip/>
-        {
-          /* 
-        <div class="row-start-3 col-start-1 col-span-2 w-64 h-8">
-          <Percents/>
-        </div>
-        <div class="row-start-4 col-start-1 col-span-2 w-64 h-8">
-          <Dollars/>
-        </div>
-        <div class="row-start-1 col-start-3 col-span-1 w-16 h-8">
-          "From"
-        </div>
-        <div class="row-start-2 col-start-3 col-span-1 w-16 h-12">
-          <Abrv/>
-        </div>
-        <div class="row-start-3 col-start-3 row-span-2 w-16 h-16">
-          <ChainPicker/>
-        </div> */
-        }
+export function From() {
+  const balance = Balance();
+
+  const from = (
+    <div class="grid grid-rows-[auto,1fr] grid-cols-[auto,1fr]">
+      {balance}
+      {
+        /* <Input/>
+      <Dollars/>
+      <Percents/> */
+      }
+      {
+        /*
+      <div class="row-start-3 col-start-3 row-span-2 w-16 h-16">
+        <ChainPicker/>
       </div>
-    );
-  },
-  {}
-)
+      */
+      }
+    </div>
+  );
+
+  return Object.assign(from, { balance });
+}
