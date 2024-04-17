64b80a7652dbf42fe587c78c2913811d8dd10284
Brad Brown <bradbrown@bradbrown.llc>
2024-04-17 19:59:51 -0400

    dzhv balance getter

---
+++ lib/bridge/madness/getters/getDzhvBalance.ts
@@ -0,0 +1,48 @@
+import { dzhv } from "lib/bridge/madness/dzhv.ts";
+import { state, loading } from "lib/bridge/madness/dzkv.ts";
+import { ejra } from "lib/bridge/madness/ejra/ejra.ts";
+import { robinController, goNext } from "lib/bridge/madness/robin.ts";
+import { TxCallObject } from "lib/bridge/madness/ejra/types/TxCallObject.ts";
+import { Signal } from "@preact/signals-core";
+import { dzkv } from "lib/dzkv.ts";
+
+dzkv.set(['loading', 'dzhvBalance'], new Signal('unload-[]'))
+dzkv.set(['state', 'dzhvBalance'], new Signal())
+
+export async function getDzhvBalance() {
+
+  // get signal, if aborted return
+  const { signal } = robinController.value
+  if (signal.aborted) return
+
+  // get requirement from state
+  const rpc = state<string>('rpc')!.value
+  const account = state<string>('account')!.value
+  const height = state<bigint>('height')!.value
+  const code = state<string>('dzhvCode')!.value
+  if (
+    !rpc
+    || !account
+    || height === undefined
+    || !code || code == '0x'
+  ) return goNext()
+
+  // 🐌
+  const to = dzhv.address
+  const input = `0x70a08231${account.slice(2).padStart(64, '0')}`
+  const txCallObject:TxCallObject = { to, input }
+  const snail = ejra.call(rpc, txCallObject, height, signal)
+  await snail.born
+  loading('dzhvBalance')!.value = 'loading-[#80ffff2b]'
+  const balance = await snail.died.then(BigInt).catch((r:Error) => r)
+  loading('dzhvBalance')!.value = signal.aborted
+    ? 'loading-[#ffbf0060]'
+    : 'unload-[]'
+
+  // handle result
+  if (signal.aborted) return
+  if (balance instanceof Error) return goNext()
+  state<bigint>('dzhvBalance')!.value = balance
+  return goNext()
+
+}
