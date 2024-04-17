9a51ca75fea4e239bfadb81758d9980fd932a83c
Brad Brown <bradbrown@bradbrown.llc>
2024-04-17 19:59:13 -0400

    simplified ejra

---
+++ lib/bridge/madness/ejra/types/Params.ts
@@ -0,0 +1,25 @@
+import { Filter, Tag, TxCallObject } from './mod.ts'
+
+export class Options {
+    signal?:AbortSignal 
+    constructor({ signal }:{ signal?:AbortSignal }={}) {
+        this.signal = signal
+    }
+}
+
+export type Params = {
+    clientVersion: [options?:Options],
+    sha3: [data:string, options?:Options],
+    chainId: [options?:Options],
+    gasPrice: [options?:Options],
+    height: [options?:Options],
+    balance: [address:string, tag:Tag, options?:Options],
+    slot: [address:string, slot:bigint, tag:Tag, options?:Options],
+    nonce: [address:string, tag:Tag, options?:Options],
+    code: [address:string, tag:Tag, options?:Options],
+    send: [data:string, options?:Options],
+    call: [txCallObject:TxCallObject, tag:Tag, options?:Options]
+    estimateGas: [txCallObject:Partial<TxCallObject>, options?:Options],
+    receipt: [hash:string, options?:Options]
+    logs: [filter:Filter, options?:Options]
+}
