6672d29e6f3db3f0601743f928d4da39f148c8fe
Brad Brown <bradbrown@bradbrown.llc>
2024-04-17 19:59:40 -0400

    balance getter

---
+++ lib/bridge/madness/getters/getBalance.ts
@@ -0,0 +1,37 @@
+import { dzkv } from "lib/mod.ts";
+import { ejra } from "lib/bridge/madness/ejra/ejra.ts";
+import { Signal } from '@preact/signals'
+import { state, loading } from "lib/bridge/madness/dzkv.ts";
+import { robinController, goNext } from "lib/bridge/madness/robin.ts";
+
+dzkv.set(['loading', 'balance'], new Signal('unload-[]'))
+dzkv.set(['state', 'balance'], new Signal())
+
+export async function getBalance() {
+
+  // get signal, if aborted return
+  const { signal } = robinController.value
+  if (signal.aborted) return
+
+  // get requirement, if missing, go next
+  const rpc = state<string>('rpc')!.value
+  const account = state<string>('account')!.value
+  const height = state<bigint>('height')!.value
+  if (!rpc || !account || height === undefined) return goNext()
+
+  // 🐌
+  const snail = ejra.balance(rpc, account, height, signal)
+  await snail.born
+  loading('balance')!.value = 'loading-[#80ffff2b]'
+  const balance = await snail.died.catch((r:Error) => r)
+  loading('balance')!.value = signal.aborted
+    ? 'loading-[#ffbf0060]'
+    : 'unload-[]'
+
+  // handle result
+  if (signal.aborted) return
+  if (balance instanceof Error) return goNext()
+  state<bigint>('balance')!.value = balance
+  return goNext()
+
+}
