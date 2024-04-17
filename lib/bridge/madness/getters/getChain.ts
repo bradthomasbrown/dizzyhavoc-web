47f595913e1926bc86721ebb64fe846de24c1a0f
Brad Brown <bradbrown@bradbrown.llc>
2024-04-17 19:59:47 -0400

    chain getter? i hardly know'er

---
+++ lib/bridge/madness/getters/getChain.ts
@@ -0,0 +1,40 @@
+import { Chain } from "lib/bridge/madness/query/types/mod.ts";
+import { Signal } from "@preact/signals-core";
+import { dzkv } from "lib/dzkv.ts";
+import { query } from "lib/bridge/madness/query/query.ts";
+import { state, loading } from "lib/bridge/madness/dzkv.ts";
+import { toad } from "lib/bridge/madness/ejra/ejra.ts";
+import { getRpc } from "lib/bridge/madness/getters/getRpc.ts";
+
+dzkv.set(['loading', 'chain'], new Signal('unload-[]'))
+dzkv.set(['state', 'chain'], new Signal())
+export const getChainController = { value: new AbortController() }
+const chainMap = new Map<number,Chain>()
+
+export async function getChain() {
+
+  // get signal, if aborted return
+  const { signal } = getChainController.value
+  if (signal.aborted) return
+
+  // get requirement from state
+  const chainId = state<number>('chainId')!.value!
+
+  let chain:Chain 
+  if (!chainMap.has(chainId)) {
+    // 🐌
+    const snail = query(toad, chainId, signal)
+    await snail.born
+    loading('chain')!.value = 'loading-[#80ffff2b]'
+    chain = await snail.died
+    chainMap.set(chainId, chain)
+  } else chain = chainMap.get(chainId)!
+  loading('chain')!.value = signal.aborted
+    ? 'loading-[#ffbf0060]'
+    : 'unload-[]'
+
+  // update state, get url
+  state<Chain>('chain')!.value = chain
+  return getRpc()
+
+}
