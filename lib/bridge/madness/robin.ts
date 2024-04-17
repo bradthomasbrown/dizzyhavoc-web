9a9e8f30cdca0ea24b91b4d0fface0d9c23cc619
Brad Brown <bradbrown@bradbrown.llc>
2024-04-17 19:59:00 -0400

    round robin evm data retrieval

---
+++ lib/bridge/madness/robin.ts
@@ -0,0 +1,18 @@
+import {
+  getHeight, getDzhvCode, getBalance
+} from "lib/bridge/madness/getters/mod.ts"
+import { getDzhvBalance } from "lib/bridge/madness/getters/getDzhvBalance.ts";
+
+export const robinController = { value: new AbortController() }
+robinController.value.abort()
+export const robinFns = [getHeight, getDzhvCode, getBalance, getDzhvBalance]
+export const robinIndex = { value: 0 }
+
+export function goNext() {
+  robinIndex.value = (robinIndex.value + 1) % robinFns.length
+  setTimeout(robinFns[robinIndex.value])
+}
+
+export function repeat() {
+  setTimeout(robinFns[robinIndex.value])
+}
