d2dca82112f6698e3b11f631ace776faf30e132a
Brad Brown <bradbrown@bradbrown.llc>
2024-04-17 19:58:31 -0400

    barrels

---
+++ lib/bridge/madness/ejra/types/mod.ts
@@ -0,0 +1,13 @@
+/**
+ * types for ejra
+ * @module
+ */
+
+export * from './EJRARequest.ts'
+export * from './Filter.ts'
+export * from './Log.ts'
+export * from './Params.ts'
+export * from './Receipt.ts'
+export * from './Tag.ts'
+export * from './Tx.ts'
+export * from './TxCallObject.ts'
---
+++ lib/bridge/madness/getters/mod.ts
@@ -0,0 +1,7 @@
+export * from "lib/bridge/madness/getters/getHeight.ts"
+export * from "lib/bridge/madness/getters/getDzhvCode.ts"
+export * from "lib/bridge/madness/getters/getBalance.ts"
+export * from "lib/bridge/madness/getters/getDzhvBalance.ts"
+export * from "lib/bridge/madness/getters/getChain.ts"
+export * from "lib/bridge/madness/getters/getRpc.ts"
+export * from "lib/bridge/madness/getters/getAccount.ts"
