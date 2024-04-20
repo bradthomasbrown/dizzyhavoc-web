79b1f7a6e54a0f95d505df632f747c6971f16369
Brad Brown <bradbrown@bradbrown.llc>
2024-04-19 20:20:38 -0400

    add cancel callback

--- islands/common/which/Which.tsx
+++ islands/common/which/Which.tsx
@@ -4,7 +4,6 @@ import { WhichSearch } from "./WhichSearch.tsx";
 import { WhichChoices } from "./WhichChoices.tsx";
 import { WhichCancel } from "./WhichCancel.tsx";
 import { Choice } from "lib/mod.ts";
-import { Button } from "components/common/mod.ts";
 /**
  * <Which/> is an island representing a "pick one from many" screen\
  * @param props.title the title of <Which/>
@@ -14,17 +13,18 @@ import { Button } from "components/common/mod.ts";
  * other than their name
  */
 export function Which<T extends unknown>(
-  { title, choices, onPick, compareFn }: {
+  { title, choices, onPick, onCancel, compareFn }: {
     title: string;
     choices: Signal<Choice<T>[]>;
     onPick: (value: T) => unknown;
+    onCancel: () => void;
     compareFn?: Parameters<Choice<T>[]["sort"]>[0];
   },
 ) {
   const filter = new Signal<string>("");
   return (
     <div class="w-full h-full max-h-full flex flex-col grow">
-      <WhichCancel />
+      <WhichCancel {...{ onCancel }} />
       <WhichTitle {...{ title }} />
       <WhichSearch {...{ filter }} />
       <WhichChoices {...{ choices, filter, onPick, compareFn }} />
