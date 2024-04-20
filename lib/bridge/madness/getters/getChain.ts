589da613ddb324dff88fd2ea72db4a241901caea
Brad Brown <bradbrown@bradbrown.llc>
2024-04-19 20:22:47 -0400

    don't abort this, doesn't make sense. is not part of robin cycle

--- lib/bridge/madness/getters/getChain.ts
+++ lib/bridge/madness/getters/getChain.ts
@@ -6,16 +6,14 @@ import { state, loading } from "lib/bridge/madness/dzkv.ts";
 import { toad } from "lib/bridge/madness/ejra/ejra.ts";
 import { getRpc } from "lib/bridge/madness/getters/getRpc.ts";
 
+if (!dzkv.get(['control', 'from', 'chain']))
+  dzkv.set(['control', 'from', 'chain'], new Signal())
+
 dzkv.set(['loading', 'chain'], new Signal('unload-[]'))
 dzkv.set(['state', 'chain'], new Signal())
-export const getChainController = { value: new AbortController() }
-const chainMap = new Map<number,Chain>()
+export const chainMap = new Map<number,Chain>()
 
 export async function getChain() {
-
-  // get signal, if aborted return
-  const { signal } = getChainController.value
-  if (signal.aborted) return
 
   // get requirement from state
   const chainId = state<number>('chainId')!.value!
@@ -23,16 +21,16 @@ export async function getChain() {
   let chain:Chain 
   if (!chainMap.has(chainId)) {
     // 🐌
-    const snail = query(toad, chainId, signal)
+    const snail = query(toad, chainId)
     await snail.born
     loading('chain')!.value = 'loading-[#80ffff2b]'
     chain = await snail.died
     chainMap.set(chainId, chain)
   } else chain = chainMap.get(chainId)!
-  if (!signal.aborted) loading('chain')!.value = 'unload-[]'
 
   // update state, get url
   state<Chain>('chain')!.value = chain
+  dzkv.get<Signal<undefined|Chain>>(['control', 'from', 'chain'])!.value = chain
   return getRpc()
 
 }
