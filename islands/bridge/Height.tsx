7b5f073db8a93f9383ba254e8d4add7887e68cb7
Brad Brown <bradbrown@bradbrown.llc>
2024-04-19 20:19:54 -0400

    new concept for a small loading bubble

--- islands/bridge/Height.tsx
+++ islands/bridge/Height.tsx
@@ -1,19 +1,43 @@
 import { loading, state } from "lib/bridge/madness/dzkv.ts";
 
 export function Height() {
-  return (
-    <div
-      class={`
-        select-none
-        absolute bottom-1 right-1
-        px-1
-        rounded-full border
-        text-sm
-        font-mono
-        ${loading('height')!.value}
-      `}
-    >
-      {state<bigint>('height')!.value}
-    </div>
-  );
+  return state<bigint>('height')?.value !== undefined
+    ? (
+      <div
+        class={`
+          select-none
+          absolute bottom-1 right-1
+          px-1
+          text-sm
+          font-mono
+          border ${loading('height')!.value}
+          rounded-full
+        `}
+      >
+        {state<bigint>('height')!.value}
+      </div>
+    )
+    : (
+        <div
+          class={`
+            absolute bottom-1 right-1
+            text-sm font-mono
+            flex
+            w-full
+            border border-transparent
+          `}
+        >
+          ​
+          <div class="w-full flex flex-row-reverse">
+            <img 
+              src="data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAA"
+              class={`
+                aspect-square h-full
+                border ${loading('height')!.value}
+                rounded-full
+              `}
+            />
+          </div>
+        </div>
+    );
 }
