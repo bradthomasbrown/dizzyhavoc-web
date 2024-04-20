26b221410440b478c7a421253630ad5db5e7cce7
Brad Brown <bradbrown@bradbrown.llc>
2024-04-19 20:19:44 -0400

    don't cancel picking if clicking outside for now

--- islands/bridge/AppContainer.tsx
+++ islands/bridge/AppContainer.tsx
@@ -11,9 +11,9 @@ export function AppContainer(props: JSX.HTMLAttributes<HTMLDivElement>) {
   const ref = createRef();
 
   function onClick(e: JSX.TargetedEvent<HTMLDivElement>) {
-    if (e.target !== ref.current) return;
-    dzkv.get<Signal<null | JSX.Element>>(["which"])!.value = null;
-    Connector.set(ConnectorState.READY);
+    // if (e.target !== ref.current) return;
+    // dzkv.get<Signal<null | JSX.Element>>(["which"])!.value = null;
+    // Connector.set(ConnectorState.READY);
   }
 
   return (
