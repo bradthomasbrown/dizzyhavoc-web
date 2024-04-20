46d02069f278d10497d8b224f31e4b060a8425a2
Brad Brown <bradbrown@bradbrown.llc>
2024-04-19 20:21:46 -0400

    don't get native coin balance (for now), add restart robin util

--- lib/bridge/madness/robin.ts
+++ lib/bridge/madness/robin.ts
@@ -1,11 +1,11 @@
 import {
-  getHeight, getDzhvCode/*, getBalance*/
+  getHeight, getDzhvCode, getActive
 } from "lib/bridge/madness/getters/mod.ts"
 import { getDzhvBalance } from "lib/bridge/madness/getters/getDzhvBalance.ts";
 
 export const robinController = { value: new AbortController() }
 robinController.value.abort()
-export const robinFns = [getHeight, getDzhvCode/*, getBalance*/, getDzhvBalance]
+export const robinFns = [getActive, getHeight, getDzhvCode, getDzhvBalance]
 export const robinIndex = { value: 0 }
 
 export function goNext() {
@@ -14,5 +14,10 @@ export function goNext() {
 }
 
 export function repeat() {
+  setTimeout(robinFns[robinIndex.value])
+}
+
+export function restart() {
+  robinIndex.value = 0
   setTimeout(robinFns[robinIndex.value])
 }
