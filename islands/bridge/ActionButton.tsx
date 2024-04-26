0e5e57ea8940e59ba7b4a79b92288f7e8522cade
Brad Brown <bradbrown@bradbrown.llc>
2024-04-26 16:01:12 -0400

    new state system

--- islands/bridge/ActionButton.tsx
+++ islands/bridge/ActionButton.tsx
@@ -1,7 +1,8 @@
 import { Connector } from "islands/common/mod.ts";
-import { state } from "lib/bridge/madness/dzkv.ts";
+import { state } from "lib/state.ts";
+import { BridgeButton } from 'islands/bridge/BridgeButton.tsx'
 
 export function ActionButton() {
-  if (!state<string>('account')!.value) return <Connector />;
-  else return <></>;
+  if (!state.account.value) return <Connector />;
+  else return <BridgeButton/>;
 }
