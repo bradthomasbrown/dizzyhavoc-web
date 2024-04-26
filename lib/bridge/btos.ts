b62db69e3a35c4c5cd20fd543af4e5c7cb94e617
Brad Brown <bradbrown@bradbrown.llc>
2024-04-26 16:07:09 -0400

    prepend a zero if fractional and no integer portion

--- lib/bridge/btos.ts
+++ lib/bridge/btos.ts
@@ -6,8 +6,8 @@ export function btos(b: bigint, decimals: number) {
   const tmpa = Array.from(tmp);
   // new tmp, decimal and last {decimal} digits
   let tmp2 = `.${tmpa.splice(-18).join("")}`;
-  // add the rest of the digits
-  tmp2 = `${tmpa.join("")}${tmp2}`;
+  // add the rest of the digits, or a zero if there are none
+  tmp2 = `${tmpa.join("") == '' ? '0' : tmpa.join("")}${tmp2}`;
   // remove trailing zeros
   tmp2 = tmp2.replace(/0*$/, "");
   // remove decimal if it's the last char
