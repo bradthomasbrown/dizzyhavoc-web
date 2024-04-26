549d092535c6044e94d584865b62a026c92f58b3
Brad Brown <bradbrown@bradbrown.llc>
2024-04-26 16:04:53 -0400

    add flipper and "to" control

--- islands/bridge/Form.tsx
+++ islands/bridge/Form.tsx
@@ -1,10 +1,13 @@
 import { FaucetLink } from "components/bridge/mod.ts";
 import { ActionButton, Control, Height, Active, Code } from "islands/bridge/mod.ts";
+import { Flipper } from "components/bridge/Flipper.tsx";
 
 export function Form() {
   return (
     <div class="select-none flex flex-col items-center gap-2">
       <Control.From />
+      <Flipper />
+      <Control.To />
       <ActionButton />
       <Height />
       <div class="absolute top-1 right-1 flex flex-col text-sm font-mono w-full gap-2">
