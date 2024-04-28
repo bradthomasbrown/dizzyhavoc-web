a23bdf0812d941ef8831e9fad4d71b85c1eac10f
Brad Brown <bradbrown@bradbrown.llc>
2024-04-28 19:21:35 -0400

    new separate viRobin, is a "subrobin"?

---
+++ lib/bridge/madness/viRobin.ts
@@ -0,0 +1,13 @@
+import { getActive } from "lib/bridge/madness/getters/getActive.ts";
+import { getEconConf } from "lib/bridge/madness/getters/getEconConf.ts";
+
+const robinFns = [getActive, getEconConf]
+const robinIndex = { value: 0 }
+
+export function run() {
+  setTimeout(robinFns[robinIndex.value])
+}
+
+export function increment() {
+  robinIndex.value = (robinIndex.value + 1) % robinFns.length
+}
