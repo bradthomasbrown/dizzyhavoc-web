246b9c97ddcc9f11ed949f439e480bd5c69e0d38
Brad Brown <bradbrown@bradbrown.llc>
2024-04-28 19:20:17 -0400

    add focus-div, SettingsCog, and Recipient to Form

--- islands/bridge/Form.tsx
+++ islands/bridge/Form.tsx
@@ -1,20 +1,36 @@
 import { FaucetLink } from "components/bridge/mod.ts";
-import { ActionButton, Control, Height, Active, Code } from "islands/bridge/mod.ts";
+import { ActionButton, Control, Height, Active, Code, SettingsCog } from "islands/bridge/mod.ts";
 import { Flipper } from "components/bridge/Flipper.tsx";
+import { Recipient } from "islands/bridge/Recipient.tsx";
+import { hidden } from "islands/bridge/BridgeButton.tsx";
 
 export function Form() {
   return (
-    <div class="select-none flex flex-col items-center gap-2">
-      <Control.From />
-      <Flipper />
-      <Control.To />
-      <ActionButton />
-      <Height />
-      <div class="absolute top-1 right-1 flex flex-col text-sm font-mono w-full gap-2">
-        <Active />
-        <Code />
+    <>
+      <div
+        class={`
+          ${hidden.value ? 'hidden' : ''}
+          absolute
+          grow
+          w-full h-full
+          bg-[#19191960]
+          z-20
+        `}
+      />
+      <div class="select-none flex flex-col items-center gap-2">
+        <SettingsCog />
+        <Control.From />
+        <Flipper />
+        <Control.To />
+        <Recipient />
+        <ActionButton />
+        <Height />
+        <div class="absolute top-1 right-1 flex flex-col text-sm font-mono w-full gap-2">
+          <Active />
+          <Code />
+        </div>
+        <FaucetLink />
       </div>
-      <FaucetLink />
-    </div>
+    </>
   );
 }
