a40640678c30d3b0ebcb6de001ee4d766c12731d
Brad Brown <bradbrown@bradbrown.llc>
2024-04-28 19:20:41 -0400

    reintroduce Recipient island

---
+++ islands/bridge/Recipient.tsx
@@ -0,0 +1,45 @@
+import { state } from "lib/state.ts";
+import { hexshort } from "lib/hexshort.ts";
+import { computed, Signal, effect } from '@preact/signals'
+
+let disposed = false
+const dispose = effect(() => state.recipient.value = state.account.value)
+
+const blurred = new Signal<boolean>(true)
+
+const uRecipient = computed(() => {
+  const recipient = state.recipient.value
+  if (!blurred.value) return recipient
+  else if (!recipient || recipient.length < 11) return ''
+  else return hexshort(recipient)
+})
+
+const uPlaceholder = computed(() => {
+  if (!state.recipient.value) return 'recipient'
+  else return hexshort(state.recipient.value)
+})
+
+export function Recipient() {
+  return (
+    <>
+      <input
+        class={`
+          ![&::-webkit-calendar-picker-indicator]:hidden
+          px-2 py-1 w-full bg-transparent text-center font-mono
+        `}
+        list="accounts"
+        onInput={e => {
+          if (!disposed) { dispose(); disposed = true }
+          state.recipient.value = e.currentTarget.value
+        }}
+        placeholder={uPlaceholder}
+        onBlur={() => blurred.value = true}
+        onFocus={() => blurred.value = false}
+        value={uRecipient.value}
+      />
+      <datalist id="accounts">
+        {state.accounts.value.map(a => <option value={a}/>)}
+      </datalist>
+    </>
+  )
+}
