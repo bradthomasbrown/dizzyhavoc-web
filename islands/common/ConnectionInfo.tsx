4cb5b0a93d1bb1c27d67950b946815090803bc17
Brad Brown <bradbrown@bradbrown.llc>
2024-04-29 13:59:32 -0400

    reintroduce connectioninfo

--- islands/common/ConnectionInfo.tsx
+++ islands/common/ConnectionInfo.tsx
@@ -1,37 +1,41 @@
-// import { computed } from "@preact/signals";
-// import { Blockie } from "../../lib/blockies/Blockie.ts";
-// import { evmVortex } from "../../lib/faucet/evmVortex/evmVortex.ts";
-// import { hexshort } from '../../lib/hexshort.ts'
+import { computed } from "@preact/signals";
+import { Blockie } from "lib/Blockie.ts";
+import { hexshort } from 'lib/hexshort.ts'
+import { chainSrc } from "lib/chainSrc.ts";
+import { state } from 'lib/state.ts'
 
-// const defaultSeed = "0xa9C5db3e478D8F2E229254ef1d7e3a8ddBf2737c";
-// const seed = computed(() => {
-//   const addresses = evmVortex.uState.addresses.value;
-//   return !addresses || addresses instanceof Error ? defaultSeed : addresses[0];
-// });
-// const blockieData = computed(() => {
-//   return new Blockie({ scale: 16, seed: seed.value }).base64();
-// });
+const defaultSeed = "0xa9C5db3e478D8F2E229254ef1d7e3a8ddBf2737c";
 
-// const hexshortSelected = computed(() => {
-//   const addresses = evmVortex.uState.addresses.value;
-//   const zeroAddress = "0x".padEnd(42, "0");
-//   return hexshort(
-//     !addresses || addresses instanceof Error ? zeroAddress : addresses[0],
-//   );
-// });
+const seed = computed(() => state.account.value ?? defaultSeed)
 
-// export function ConnectionInfo() {
-//   return (
-//     <div class="absolute top-3 left-3 flex flex-row">
-//       <img
-//         class="select-none size-[1.4rem] rounded-sm mr-1"
-//         src={blockieData}
-//         title={seed}
-//         alt="blockie image"
-//       />
-//       <div class="font-[14px] mb-2">
-//         {hexshortSelected}
-//       </div>
-//     </div>
-//   );
-// }
+const blockieData = computed(() => {
+  return new Blockie({ scale: 16, seed: seed.value }).base64();
+});
+
+const hexshortSelected = computed(() => hexshort(state.account.value ?? ''))
+
+const hidden = computed(() => {
+  return !state.account.value || state.from.chainId.value === undefined
+})
+
+export function ConnectionInfo() {
+  return (
+    <div
+      class={`
+        absolute
+        top-3 left-3 
+        flex flex-row ${hidden.value ? 'hidden' : ''}
+      `
+    }>
+      <img
+        class="select-none size-[1.4rem] rounded-sm mr-1"
+        src={blockieData}
+        title={seed}
+        alt="blockie image"
+      />
+      <div class="font-[14px] mb-2">
+        {hexshortSelected}
+      </div>
+    </div>
+  );
+}
