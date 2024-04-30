88d2ec4103fc7a99b6b0b38661fe687a18df61f3
Brad Brown <bradbrown@bradbrown.llc>
2024-04-30 14:18:06 -0400

    faucet ActionButton modeled on bridge

--- islands/faucet/ActionButton.tsx
+++ islands/faucet/ActionButton.tsx
@@ -1,18 +1,8 @@
-// import { Button } from "../../components/common/Button.tsx";
-// import { drink } from "../../lib/faucet/drink.ts";
-// import { Connector, status } from "../common/Connector.tsx";
+import { Connector } from "islands/faucet/Connector.tsx";
+import { state } from "lib/faucet/state.ts";
+import { FaucetButton } from "islands/faucet/FaucetButton.tsx";
 
-// export function ActionButton() {
-//   if (status.value == "Connected") {
-//     return (
-//       <Button
-//         addClass="text-[#3d3d3d] dark:text-[#ccb286]"
-//         disabled={false}
-//         onClick={() => drink()}
-//         wiggle={false}
-//       >
-//         Get DZHV
-//       </Button>
-//     );
-//   } else return <Connector addClass="order-3" />;
-// }
+export function ActionButton() {
+  if (!state.account.value) return <Connector />;
+  else return <FaucetButton />;
+}
