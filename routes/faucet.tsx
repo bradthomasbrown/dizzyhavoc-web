84c37ece8a1bc01ff5ef03ecbc43403d13c30de5
Brad Brown <bradbrown@bradbrown.llc>
2024-04-30 14:20:44 -0400

    rewrite faucet route modeled on bridge route

--- routes/faucet.tsx
+++ routes/faucet.tsx
@@ -1,27 +1,24 @@
-// import { Form } from "../components/faucet/Form.tsx";
+import { ActiveForm, AppContainer } from "islands/faucet/mod.ts";
+import { Art, Body } from "components/common/mod.ts";
 
-// export default function Faucet() {
-//   return (
-//     <>
-//       {/*grow to keep footer at bottom, includes title and app form*/}
-//       <div class="flex w-full grow flex-col items-center">
-//         <div className="min-w-[360px] flex-col sm:min-w-[500px] bg-blur2 shadow-xl rounded-xl flex min-h-[25rem] my-auto">
-//           <div class="lg:text-[1.8rem] unselectable text-[1.5rem] font-[Poppins] font-medium dark:text-[#d2d2d2] text-[#282828] pl-2">
-//             testnet faucet
-//           </div>
-
-//           <Form />
-//         </div>
-//       </div>
-//     </>
-//   );
-// }
-import { AppContainer } from "../components/faucet/AppContainer.tsx";
 
 export default function Home() {
   return (
     <div class="text-[#2c2c2c] dark:text-[#EAEAEA] font-[Poppins]">
-      <AppContainer />
+      <AppContainer>
+        <Body.Container>
+          <Body.Title text="Faucet" />
+          <Body>
+            <Art.Container>
+              <Art
+                src="/misc/dzhv-art-blue-yellow-busy.jpg"
+                rotate="rotate-[45deg]"
+              /> 
+            </Art.Container>
+            <ActiveForm />
+          </Body>
+        </Body.Container>
+      </AppContainer>
     </div>
   );
 }
